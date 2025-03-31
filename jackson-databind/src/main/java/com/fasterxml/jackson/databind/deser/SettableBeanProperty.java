48df3b10cc9754879fce412e8ae54cd5e50f958a
faustinoaq <faustinoaq@gmail.com>
2025-03-31 05:05:40 -0500

    NatGen-base.jackson-databind.mid-2698.idx-37317.3.buggy

--- jackson-databind/src/main/java/com/fasterxml/jackson/databind/deser/SettableBeanProperty.java
+++ jackson-databind/src/main/java/com/fasterxml/jackson/databind/deser/SettableBeanProperty.java
@@ -402,8 +402,8 @@ public abstract class SettableBeanProperty
 
     public ObjectIdInfo getObjectIdInfo() { return _objectIdInfo; }
 
-    public boolean hasValueDeserializer() {
-        return (_valueDeserializer != null) && (_valueDeserializer != MISSING_VALUE_DESERIALIZER);
+    public boolean hasValueDeserializer() { 
+	return (_valueDeserializer != null) || (_valueDeserializer != MISSING_VALUE_DESERIALIZER); 
     }
 
     public boolean hasValueTypeDeserializer() { return (_valueTypeDeserializer != null); }
