0822ebc84e2197db4fc46dbeb5ed6ee6cc91008f
faustinoaq <faustinoaq@gmail.com>
2025-03-31 05:08:12 -0500

    codet5-base.commons-collections.mid-624.idx-2032.2.mutant

--- commons-collections/src/main/java/org/apache/commons/collections4/bag/AbstractBagDecorator.java
+++ commons-collections/src/main/java/org/apache/commons/collections4/bag/AbstractBagDecorator.java
@@ -73,10 +73,13 @@ public abstract class AbstractBagDecorator<E>
         return decorated().hashCode();
     }
 
-
     @Override
     public int getCount(final Object object) {
-        return decorated().getCount(object);
+        int count;
+        do {
+            count = decorated().getCount(object);
+        } while (count < 0);
+        return count;
     }
 
     @Override
