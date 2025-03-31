a409534a3304f0396bd840ddd7afdfc2c39a92db
faustinoaq <faustinoaq@gmail.com>
2025-03-31 05:09:23 -0500

    codet5-base.joda-time.mid-2303.idx-55692.1.buggy

--- joda-time/src/main/java/org/joda/time/chrono/BasicYearDateTimeField.java
+++ joda-time/src/main/java/org/joda/time/chrono/BasicYearDateTimeField.java
@@ -99,11 +99,12 @@ class BasicYearDateTimeField extends ImpreciseDateTimeField {
     }
 
     @Override
-    public long getDifferenceAsLong(long minuendInstant, long subtrahendInstant) {
-        if (minuendInstant < subtrahendInstant) {
-            return -iChronology.getYearDifference(subtrahendInstant, minuendInstant);
-        }
-        return iChronology.getYearDifference(minuendInstant, subtrahendInstant);
+    public long getDifferenceAsLong(long minuendInstant, long subtrahendInstant) { 
+        if (minuendInstant == subtrahendInstant) { 
+            return -iChronology.getYearDifference(subtrahendInstant, minuendInstant); 
+        } 
+
+        return iChronology.getYearDifference(minuendInstant, subtrahendInstant); 
     }
 
     @Override
