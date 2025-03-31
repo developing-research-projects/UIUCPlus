a8e7a4f8cc2b7d44d3446123adc573ff396d9840
faustinoaq <faustinoaq@gmail.com>
2025-03-31 05:08:39 -0500

    codet5-base.commons-lang.mid-3379.idx-18218.3.mutant

--- commons-lang/src/main/java/org/apache/commons/lang3/time/FormatCache.java
+++ commons-lang/src/main/java/org/apache/commons/lang3/time/FormatCache.java
@@ -230,14 +230,12 @@ abstract class FormatCache<F extends Format> {
             if (obj == null) {
                 return false;
             }
-            if (getClass() != obj.getClass()) {
-                return false;
+            if (getClass() == obj.getClass()) {
+                final ArrayKey other = (ArrayKey) obj;
+                return Arrays.deepEquals(keys, other.keys);
             }
-            final ArrayKey other = (ArrayKey) obj;
-            return Arrays.deepEquals(keys, other.keys);
+            return false;
         }
-
-
     }
 
 }
