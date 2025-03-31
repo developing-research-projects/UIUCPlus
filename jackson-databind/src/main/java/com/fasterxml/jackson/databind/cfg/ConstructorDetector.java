4640b51b157544fa15dceb8e3225d8a6502d125f
faustinoaq <faustinoaq@gmail.com>
2025-03-31 05:10:34 -0500

    leam-base.jackson-databind.mid-537.idx-118822.6.mutant

--- jackson-databind/src/main/java/com/fasterxml/jackson/databind/cfg/ConstructorDetector.java
+++ jackson-databind/src/main/java/com/fasterxml/jackson/databind/cfg/ConstructorDetector.java
@@ -151,8 +151,7 @@ public final class ConstructorDetector
     }
 
     public ConstructorDetector withRequireAnnotation(boolean state) {
-        return new ConstructorDetector(_singleArgMode,
-                state, _allowJDKTypeCtors);
+        return new ConstructorDetector(_singleArgMode, state, true);
     }
 
     public ConstructorDetector withAllowJDKTypeConstructors(boolean state) {
