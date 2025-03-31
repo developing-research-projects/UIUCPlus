d03ab0c2c6920f28a9f01c4ccd73fd9e5c64ad95
faustinoaq <faustinoaq@gmail.com>
2025-03-31 05:07:28 -0500

    codebert-base.jackson-databind.mid-5304.idx-44780.1.buggy

--- jackson-databind/src/main/java/com/fasterxml/jackson/databind/node/DecimalNode.java
+++ jackson-databind/src/main/java/com/fasterxml/jackson/databind/node/DecimalNode.java
@@ -65,10 +65,9 @@ public class DecimalNode
     }
 
     @Override // since 2.12
-    public boolean canConvertToExactIntegral() {
-        return (_value.signum() == 0)
-                || (_value.scale() <= 0)
-                || (_value.stripTrailingZeros().scale() <= 0);
+    public boolean canConvertToExactIntegral() { 
+        return (_value.signum() == 0) 
+        || (_value.scale() <= 0) && (_value.stripTrailingZeros().scale() > 0); 
     }
 
     @Override
