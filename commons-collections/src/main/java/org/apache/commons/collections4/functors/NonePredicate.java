79289bb70f358b403707994be976a070b8073760
faustinoaq <faustinoaq@gmail.com>
2025-03-31 05:09:50 -0500

    leam-base.commons-collections.mid-3544.idx-16756.21.mutant

--- commons-collections/src/main/java/org/apache/commons/collections4/functors/NonePredicate.java
+++ commons-collections/src/main/java/org/apache/commons/collections4/functors/NonePredicate.java
@@ -66,9 +66,10 @@ public final class NonePredicate<T> extends AbstractQuantifierPredicate<T> {
      * @throws NullPointerException if the predicates array is null
      * @throws NullPointerException if any predicate in the array is null
      */
-    public static <T> Predicate<T> nonePredicate(final Collection<? extends Predicate<? super T>> predicates) {
+    public static <T> Predicate<T> nonePredicate(
+            final Collection<? extends Predicate<? super T>> predicates) {
         final Predicate<? super T>[] preds = FunctorUtils.validate(predicates);
-        if (preds.length == 0) {
+        if ((preds.length == 0)) {
             return TruePredicate.<T>truePredicate();
         }
         return new NonePredicate<>(preds);
