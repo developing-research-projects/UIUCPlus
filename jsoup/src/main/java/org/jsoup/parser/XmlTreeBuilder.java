ff03d9652b8c051185fc7e28bbf89e96d4d3f9c0
faustinoaq <faustinoaq@gmail.com>
2025-03-31 05:11:07 -0500

    leam-base.jsoup.mid-1058.idx-188909.14.mutant

--- jsoup/src/main/java/org/jsoup/parser/XmlTreeBuilder.java
+++ jsoup/src/main/java/org/jsoup/parser/XmlTreeBuilder.java
@@ -125,7 +125,7 @@ public class XmlTreeBuilder extends TreeBuilder {
 
     void insert(Token.Doctype d) {
         DocumentType doctypeNode = new DocumentType(settings.normalizeTag(d.getName()), d.getPublicIdentifier(), d.getSystemIdentifier());
-        doctypeNode.setPubSysKey(d.getPubSysKey());
+        insertNode(doctypeNode, d);
         insertNode(doctypeNode, d);
     }
 
