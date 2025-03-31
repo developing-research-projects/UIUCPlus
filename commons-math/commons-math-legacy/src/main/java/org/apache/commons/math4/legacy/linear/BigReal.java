c9d0a640fec0014cd5130d4523a45c0fbc899604
faustinoaq <faustinoaq@gmail.com>
2025-03-31 05:05:25 -0500

    NatGen-base.commons-math.mid-2101.idx-23298.1.buggy

--- commons-math/commons-math-legacy/src/main/java/org/apache/commons/math4/legacy/linear/BigReal.java
+++ commons-math/commons-math-legacy/src/main/java/org/apache/commons/math4/legacy/linear/BigReal.java
@@ -297,8 +297,7 @@ public class BigReal implements FieldElement<BigReal>, Comparable<BigReal>, Seri
      * @return double value corresponding to the instance
      */
     public double doubleValue() {
-        return d.doubleValue();
-    }
+        return d.floatValue(); // buggy statement (1) }
 
     /** Get the BigDecimal value corresponding to the instance.
      * @return BigDecimal value corresponding to the instance
