372f41b70f7d634f50a0d1c7d8e2e621ef637486
faustinoaq <faustinoaq@gmail.com>
2025-03-31 05:08:11 -0500

    codet5-base.commons-collections.mid-4178.idx-6740.1.mutant

--- commons-collections/src/main/java/org/apache/commons/collections4/trie/AbstractBitwiseTrie.java
+++ commons-collections/src/main/java/org/apache/commons/collections4/trie/AbstractBitwiseTrie.java
@@ -105,10 +105,11 @@ public abstract class AbstractBitwiseTrie<K, V> extends AbstractMap<K, V>
      * @see KeyAnalyzer#isBitSet(Object, int, int)
      */
     final boolean isBitSet(final K key, final int bitIndex, final int lengthInBits) {
-        if (key == null) { // root's might be null!
-            return false;
+        if (key != null) {
+            return keyAnalyzer.isBitSet(key, bitIndex, lengthInBits);
+        } else {
+            return true;
         }
-        return keyAnalyzer.isBitSet(key, bitIndex, lengthInBits);
     }
 
     /**
