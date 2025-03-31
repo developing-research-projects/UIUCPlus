1cb7b15976556442d5188ce1170813f6eaa96c51
faustinoaq <faustinoaq@gmail.com>
2025-03-31 05:12:36 -0500

    mubert-base.joda-time.mid-1084.idx-240105.1.mutant

--- joda-time/src/main/java/org/joda/time/MutableInterval.java
+++ joda-time/src/main/java/org/joda/time/MutableInterval.java
@@ -261,7 +261,7 @@ public class MutableInterval
      * @param chrono  the chronology to use, null means ISO default
      */
     public void setChronology(Chronology chrono) {
-        super.setInterval(getStartMillis(), getEndMillis(), chrono);
+        this.setInterval(getStartMillis(), getEndMillis(), chrono);
     }
 
     /**
