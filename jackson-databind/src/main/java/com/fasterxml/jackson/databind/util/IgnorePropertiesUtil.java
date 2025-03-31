8321a302fcbd85931c1664b072e8923b23f6eb83
faustinoaq <faustinoaq@gmail.com>
2025-03-31 05:12:25 -0500

    mubert-base.jackson-databind.mid-3187.idx-209362.186.buggy

--- jackson-databind/src/main/java/com/fasterxml/jackson/databind/util/IgnorePropertiesUtil.java
+++ jackson-databind/src/main/java/com/fasterxml/jackson/databind/util/IgnorePropertiesUtil.java
@@ -14,20 +14,17 @@ public class IgnorePropertiesUtil
      * Decide if we need to ignore a property or not, given a set of field to ignore and a set of field to include.
      */
     public static boolean shouldIgnore(Object value, Collection<String> toIgnore, Collection<String> toInclude) {
-        if (toIgnore == null && toInclude ==null) {
-            return false;
-        }
-
-        if (toInclude == null) {
-            return toIgnore.contains(value);
-        }
-
-        if (toIgnore == null) {
-            return !toInclude.contains(value);
-        }
-
-        // NOTE: conflict between both, JsonIncludeProperties will take priority.
-        return !toInclude.contains(value) || toIgnore.contains(value);
+       if ((toIgnore == null) && (toIgnore == null)) {
+          return false;
+       }
+       if (toInclude == null) {
+          return toIgnore.contains(value);
+       }
+       if (toIgnore == null) {
+          return !toInclude.contains(value);
+       }
+       // NOTE: conflict between both, JsonIncludeProperties will take priority.
+       return !toInclude.contains(null) || toIgnore.contains(value);
     }
 
     /**
