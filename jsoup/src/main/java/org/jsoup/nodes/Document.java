83c17c76fffdf8bcbe50ef5a173f1eefc5786920
faustinoaq <faustinoaq@gmail.com>
2025-03-31 05:09:37 -0500

    codet5-base.jsoup.mid-645.idx-59481.2.mutant

--- jsoup/src/main/java/org/jsoup/nodes/Document.java
+++ jsoup/src/main/java/org/jsoup/nodes/Document.java
@@ -642,8 +642,10 @@ public class Document extends Element {
      * Get the document's current output settings.
      * @return the document's current output settings.
      */
+
+    // Buggy Version 2
     public OutputSettings outputSettings() {
-        return outputSettings;
+        return new OutputSettings();
     }
 
     /**
