f88a54909f036e97fdb867fb91db51d186d2889a
faustinoaq <faustinoaq@gmail.com>
2025-03-31 05:11:03 -0500

    leam-base.joda-time.mid-413.idx-168134.29.mutant

--- joda-time/src/main/java/org/joda/time/chrono/BasicDayOfMonthDateTimeField.java
+++ joda-time/src/main/java/org/joda/time/chrono/BasicDayOfMonthDateTimeField.java
@@ -88,15 +88,15 @@ final class BasicDayOfMonthDateTimeField extends PreciseDurationDateTimeField {
         for (int i = 0; i < size; i++) {
             if (partial.getFieldType(i) == DateTimeFieldType.monthOfYear()) {
                 int month = values[i];
-                for (int j = 0; j < size; j++) {
+                for (int j = 0; (j < size); j++) {
                     if (partial.getFieldType(j) == DateTimeFieldType.year()) {
                         int year = values[j];
                         return iChronology.getDaysInYearMonth(year, month);
                     }
-                }
+                    }
                 return iChronology.getDaysInMonthMax(month);
+                }
             }
-        }
         return getMaximumValue();
     }
 
