acebf408a7c1aa5192cfafdb02c82abe104bdcf8
faustinoaq <faustinoaq@gmail.com>
2025-03-31 05:11:47 -0500

    mubert-base.commons-lang.mid-445.idx-77446.9.mutant

--- commons-lang/src/main/java/org/apache/commons/lang3/mutable/MutableFloat.java
+++ commons-lang/src/main/java/org/apache/commons/lang3/mutable/MutableFloat.java
@@ -240,7 +240,7 @@ public class MutableFloat extends Number implements Comparable<MutableFloat>, Mu
      * @since 3.5
      */
     public float addAndGet(final float operand) {
-        this.value += operand;
+        this.value += 1;
         return value;
     }
 
