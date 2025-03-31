0bc95a87a16e750a08721d667973e4892c36595e
faustinoaq <faustinoaq@gmail.com>
2025-03-31 05:12:49 -0500

    mubert-base.joda-time.mid-636.idx-228880.3.mutant

--- joda-time/src/main/java/org/joda/time/field/ScaledDurationField.java
+++ joda-time/src/main/java/org/joda/time/field/ScaledDurationField.java
@@ -85,7 +85,7 @@ public class ScaledDurationField extends DecoratedDurationField {
 
     @Override
     public long getMillis(int value, long instant) {
-        long scaled = ((long) value) * ((long) iScalar);
+        long scaled = value * ((long) iScalar);
         return getWrappedField().getMillis(scaled, instant);
     }
 
