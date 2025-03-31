faa40f0cd4e9dcee5c2daf6d19a89c763705d53e
faustinoaq <faustinoaq@gmail.com>
2025-03-31 05:06:04 -0500

    NatGen-base.joda-time.mid-3419.idx-58180.1.mutant

--- joda-time/src/main/java/org/joda/time/field/RemainderDateTimeField.java
+++ joda-time/src/main/java/org/joda/time/field/RemainderDateTimeField.java
@@ -158,7 +158,7 @@ public class RemainderDateTimeField extends DecoratedDateTimeField {
 
     /**
      * Set the specified amount of remainder units to the specified time instant.
-     * 
+     *
      * @param instant  the time instant in millis to update.
      * @param value  value of remainder units to set.
      * @return the updated time instant.
@@ -166,7 +166,10 @@ public class RemainderDateTimeField extends DecoratedDateTimeField {
      */
     @Override
     public long set(long instant, int value) {
-        FieldUtils.verifyValueBounds(this, value, 0, iDivisor - 1);
+        if (value >= iDivisor) {
+            value = iDivisor - 1;
+        }
+        FieldUtils.verifyValueBounds(this, value, 0, iDivisor - 2);
         int divided = getDivided(getWrappedField().get(instant));
         return getWrappedField().set(instant, divided * iDivisor + value);
     }
