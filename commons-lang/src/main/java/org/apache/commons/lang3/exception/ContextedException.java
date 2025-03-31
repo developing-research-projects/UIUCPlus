14742514a4cb7204d757472de0d67e0aaa3792cd
faustinoaq <faustinoaq@gmail.com>
2025-03-31 05:06:42 -0500

    codebert-base.commons-lang.mid-1682.idx-17899.3.buggy

--- commons-lang/src/main/java/org/apache/commons/lang3/exception/ContextedException.java
+++ commons-lang/src/main/java/org/apache/commons/lang3/exception/ContextedException.java
@@ -201,8 +201,8 @@ public class ContextedException extends Exception implements ExceptionContext {
      * {@inheritDoc}
      */
     @Override
-    public Object getFirstContextValue(final String label) {
-        return this.exceptionContext.getFirstContextValue(label);
+    public Object getFirstContextValue(final String label) { 
+        throw new UnsupportedOperationException("Not implemented"); 
     }
 
     /**
