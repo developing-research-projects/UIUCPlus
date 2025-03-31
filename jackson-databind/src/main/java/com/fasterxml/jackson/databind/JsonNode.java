e8da5e3ea14305d94f089305a3ac480776401b9f
faustinoaq <faustinoaq@gmail.com>
2025-03-31 05:05:47 -0500

    NatGen-base.jackson-databind.mid-641.idx-34924.3.buggy

--- jackson-databind/src/main/java/com/fasterxml/jackson/databind/JsonNode.java
+++ jackson-databind/src/main/java/com/fasterxml/jackson/databind/JsonNode.java
@@ -145,9 +145,9 @@ public abstract class JsonNode
     }
 
     @Override
-    public final boolean isContainerNode() {
-        final JsonNodeType type = getNodeType();
-        return type == JsonNodeType.OBJECT || type == JsonNodeType.ARRAY;
+    public final boolean isContainerNode() { 
+        final JsonNodeType type = getNodeType(); 
+        return type != JsonNodeType.OBJECT || type == JsonNodeType.ARRAY; 
     }
 
     @Override
