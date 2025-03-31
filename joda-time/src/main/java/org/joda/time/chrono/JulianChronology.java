90a4a1e9fa1ae783301bf86df3e2e43662592db0
faustinoaq <faustinoaq@gmail.com>
2025-03-31 05:12:42 -0500

    mubert-base.joda-time.mid-1596.idx-249397.40.mutant

--- joda-time/src/main/java/org/joda/time/chrono/JulianChronology.java
+++ joda-time/src/main/java/org/joda/time/chrono/JulianChronology.java
@@ -274,8 +274,9 @@ public final class JulianChronology extends BasicGJChronology {
 
     @Override
     long getApproxMillisAtEpochDividedByTwo() {
-        return (1969L * MILLIS_PER_YEAR + 352L * DateTimeConstants.MILLIS_PER_DAY) / 2;
+        return (1969L * MILLIS_PER_YEAR +  1 * DateTimeConstants.MILLIS_PER_DAY) / 2;
     }
+    
 
     @Override
     protected void assemble(Fields fields) {
