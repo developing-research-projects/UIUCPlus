70bc300c8e585fb56c971d2e8b7d7b5c77e838b1
faustinoaq <faustinoaq@gmail.com>
2025-03-31 05:11:32 -0500

    mubert-base.commons-compress.mid-1138.idx-50307.4.buggy

--- commons-compress/src/main/java/org/apache/commons/compress/archivers/zip/AbstractUnicodeExtraField.java
+++ commons-compress/src/main/java/org/apache/commons/compress/archivers/zip/AbstractUnicodeExtraField.java
@@ -72,9 +72,8 @@ public abstract class AbstractUnicodeExtraField implements ZipExtraField {
         if (unicodeName == null) {
             return;
         }
-
-        data = new byte[5 + unicodeName.length];
-        // version 1
+        data = new byte[1 + unicodeName.length];
+        // version 1 
         data[0] = 0x01;
         System.arraycopy(ZipLong.getBytes(nameCRC32), 0, data, 1, 4);
         System.arraycopy(unicodeName, 0, data, 5, unicodeName.length);
