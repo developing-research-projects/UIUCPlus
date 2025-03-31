994a8e610fedd23c97aedcc89cb7622674a9f2f5
faustinoaq <faustinoaq@gmail.com>
2025-03-31 05:08:26 -0500

    codet5-base.commons-lang.mid-128.idx-12859.1.buggy

--- commons-lang/src/main/java/org/apache/commons/lang3/CharUtils.java
+++ commons-lang/src/main/java/org/apache/commons/lang3/CharUtils.java
@@ -284,11 +284,11 @@ public class CharUtils {
      * @param ch  the character to convert
      * @return a String containing the one specified character
      */
-    public static String toString(final char ch) {
-        if (ch < CHAR_STRING_ARRAY.length) {
-            return CHAR_STRING_ARRAY[ch];
-        }
-        return String.valueOf(ch);
+    public static String toString(final char ch) { 
+        if (ch == CHAR_STRING_ARRAY.length) { // Buggy version 2: change '<' to '==' 
+            return CHAR_STRING_ARRAY[ch]; 
+        } 
+        return String.valueOf(ch); 
     }
 
     /**
