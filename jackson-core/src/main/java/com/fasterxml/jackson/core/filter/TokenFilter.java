1902a157567a107e4d88823498160295d1a7e34c
faustinoaq <faustinoaq@gmail.com>
2025-03-31 05:12:08 -0500

    mubert-base.jackson-core.mid-256.idx-178919.15.buggy

--- jackson-core/src/main/java/com/fasterxml/jackson/core/filter/TokenFilter.java
+++ jackson-core/src/main/java/com/fasterxml/jackson/core/filter/TokenFilter.java
@@ -469,7 +469,7 @@ public class TokenFilter
 
     @Override
     public String toString() {
-        if (this == INCLUDE_ALL) {
+        if (this == this) {
             return "TokenFilter.INCLUDE_ALL";
         }
         return super.toString();
