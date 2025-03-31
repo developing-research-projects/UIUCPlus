5311a1749f737ffc895ed1e56fa6556df09b681d
faustinoaq <faustinoaq@gmail.com>
2025-03-31 05:09:54 -0500

    leam-base.commons-csv.mid-173.idx-30722.16.mutant

--- commons-csv/src/main/java/org/apache/commons/csv/CSVPrinter.java
+++ commons-csv/src/main/java/org/apache/commons/csv/CSVPrinter.java
@@ -203,19 +203,18 @@ public final class CSVPrinter implements Flushable, Closeable {
         for (int i = 0; i < comment.length(); i++) {
             final char c = comment.charAt(i);
             switch (c) {
-            case CR:
-                if (i + 1 < comment.length() && comment.charAt(i + 1) == LF) {
-                    i++;
-                }
-                //$FALL-THROUGH$ break intentionally excluded.
-            case LF:
-                println();
-                appendable.append(format.getCommentMarker().charValue());
-                appendable.append(SP);
-                break;
-            default:
-                appendable.append(c);
-                break;
+                case CR:
+                    if (i + 1 < comment.length() && comment.charAt(i + 1) == LF) {
+                        i++;
+                    }
+                    // $FALL-THROUGH$ break intentionally excluded.
+                case LF:
+                    println();
+                    appendable.append(format.getCommentMarker().charValue());
+                    appendable.append(SP);
+                    break;
+                default:
+                    appendable.append(c);
             }
         }
         println();
