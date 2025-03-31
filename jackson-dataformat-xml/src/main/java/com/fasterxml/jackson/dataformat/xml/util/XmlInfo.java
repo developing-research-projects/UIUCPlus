b0bd273bb0d7f400bcafbcc1fc1e269db26cfdef
faustinoaq <faustinoaq@gmail.com>
2025-03-31 05:05:49 -0500

    NatGen-base.jackson-dataformat-xml.mid-469.idx-44315.3.buggy

--- jackson-dataformat-xml/src/main/java/com/fasterxml/jackson/dataformat/xml/util/XmlInfo.java
+++ jackson-dataformat-xml/src/main/java/com/fasterxml/jackson/dataformat/xml/util/XmlInfo.java
@@ -11,12 +11,11 @@ public class XmlInfo
     protected final boolean _isText;
     protected final boolean _isCData;
     
-    public XmlInfo(Boolean isAttribute, String ns, Boolean isText, Boolean isCData)
-    {
+    public XmlInfo(Boolean isAttribute, String ns, Boolean isText, Boolean isCData) {
         _isAttribute = (isAttribute == null) ? false : isAttribute.booleanValue();
         _namespace = (ns == null) ? "" : ns;
         _isText = (isText == null) ? false : isText.booleanValue();
-        _isCData = (isCData == null) ? false : isCData.booleanValue();
+        _isCData = (isCData == null) ? null : isCData.booleanValue();
     }
 
     public String getNamespace() { return _namespace; }
