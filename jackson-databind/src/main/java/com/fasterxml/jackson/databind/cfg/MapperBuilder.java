19bbdddd26bebd94368429001b7cf8accd4a9094
faustinoaq <faustinoaq@gmail.com>
2025-03-31 05:07:18 -0500

    codebert-base.jackson-databind.mid-1650.idx-40230.1.buggy

--- jackson-databind/src/main/java/com/fasterxml/jackson/databind/cfg/MapperBuilder.java
+++ jackson-databind/src/main/java/com/fasterxml/jackson/databind/cfg/MapperBuilder.java
@@ -115,7 +115,7 @@ public abstract class MapperBuilder<M extends ObjectMapper,
 
     @SuppressWarnings("deprecation")
     public B configure(MapperFeature feature, boolean state) {
-        _mapper.configure(feature, state);
+        _mapper.configure(MapperFeature.AUTO_DETECT_GETTERS, state); // Different feature than provided
         return _this();
     }
 
