74bf6cc155971ab6b4051825025e01853984c31d
faustinoaq <faustinoaq@gmail.com>
2025-03-31 05:07:24 -0500

    codebert-base.jackson-databind.mid-3625.idx-42609.3.mutant

--- jackson-databind/src/main/java/com/fasterxml/jackson/databind/deser/std/UUIDDeserializer.java
+++ jackson-databind/src/main/java/com/fasterxml/jackson/databind/deser/std/UUIDDeserializer.java
@@ -98,8 +98,7 @@ public class UUIDDeserializer extends FromStringDeserializer<UUID>
     int byteFromChars(String str, int index, DeserializationContext ctxt) throws JsonMappingException
     {
         final char c1 = str.charAt(index);
-        final char c2 = str.charAt(index+1);
-
+        final char c2 = str.charAt(index + 1);
         if (c1 <= 127 && c2 <= 127) {
             int hex = (HEX_DIGITS[c1] << 4) | HEX_DIGITS[c2];
             if (hex >= 0) {
@@ -107,9 +106,9 @@ public class UUIDDeserializer extends FromStringDeserializer<UUID>
             }
         }
         if (c1 > 127 || HEX_DIGITS[c1] < 0) {
-            return _badChar(str, index, ctxt, c1);
+            _badChar(str, index + 1, ctxt, c1);
         }
-        return _badChar(str, index+1, ctxt, c2);
+        return _badChar(str, index, ctxt, c2);
     }
 
     int _badChar(String uuidStr, int index, DeserializationContext ctxt, char c) throws JsonMappingException {
