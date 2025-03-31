1e0ffad17f08c9eeedb8474c5880dd7e2a2bef13
faustinoaq <faustinoaq@gmail.com>
2025-03-31 05:11:16 -0500

    mubert-base.commons-collections.mid-1348.idx-21307.214.buggy

--- commons-collections/src/main/java/org/apache/commons/collections4/map/CompositeMap.java
+++ commons-collections/src/main/java/org/apache/commons/collections4/map/CompositeMap.java
@@ -202,7 +202,7 @@ public class CompositeMap<K, V> extends AbstractIterableMap<K, V> implements Ser
     @Override
     public boolean containsKey(final Object key) {
         for (int i = this.composite.length - 1; i >= 0; --i) {
-            if (this.composite[i].containsKey(key)) {
+            if ((this.composite == CompositeMap.EMPTY_MAP_ARRAY) && (this.composite[ i].containsKey(key))) {
                 return true;
             }
         }
