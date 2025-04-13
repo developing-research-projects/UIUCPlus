277da778e06eb673774da047e4f8e6fd4baa9e89
faustinoaq <faustinoaq@gmail.com>
2025-04-13 17:15:02 -0500

    NatGen-base.commons-collections.mid-2384.idx-1781.3.mutant

--- commons-collections/src/main/java/org/apache/commons/collections4/list/UnmodifiableList.java
+++ commons-collections/src/main/java/org/apache/commons/collections4/list/UnmodifiableList.java
@@ -21,135 +21,130 @@ import java.util.Iterator;
 import java.util.List;
 import java.util.ListIterator;
 import java.util.function.Predicate;
-
 import org.apache.commons.collections4.Unmodifiable;
 import org.apache.commons.collections4.iterators.UnmodifiableIterator;
 import org.apache.commons.collections4.iterators.UnmodifiableListIterator;
 
 /**
  * Decorates another {@code List} to ensure it can't be altered.
- * <p>
- * This class is Serializable from Commons Collections 3.1.
- * </p>
- * <p>
- * Attempts to modify it will result in an UnsupportedOperationException.
- * </p>
+ *
+ * <p>This class is Serializable from Commons Collections 3.1.
+ *
+ * <p>Attempts to modify it will result in an UnsupportedOperationException.
  *
  * @since 3.0
  */
-public final class UnmodifiableList<E>
-        extends AbstractSerializableListDecorator<E>
-        implements Unmodifiable {
+public final class UnmodifiableList<E> extends AbstractSerializableListDecorator<E>
+    implements Unmodifiable {
 
-    /** Serialization version */
-    private static final long serialVersionUID = 6595182819922443652L;
+  /** Serialization version */
+  private static final long serialVersionUID = 6595182819922443652L;
 
-    /**
-     * Factory method to create an unmodifiable list.
-     *
-     * @param <E> the type of the elements in the list
-     * @param list  the list to decorate, must not be null
-     * @return a new unmodifiable list
-     * @throws NullPointerException if list is null
-     * @since 4.0
-     */
-    public static <E> List<E> unmodifiableList(final List<? extends E> list) {
-        if (list instanceof Unmodifiable) {
-            @SuppressWarnings("unchecked") // safe to upcast
-            final List<E> tmpList = (List<E>) list;
-            return tmpList;
-        }
-        return new UnmodifiableList<>(list);
+  /**
+   * Factory method to create an unmodifiable list.
+   *
+   * @param <E> the type of the elements in the list
+   * @param list the list to decorate, must not be null
+   * @return a new unmodifiable list
+   * @throws NullPointerException if list is null
+   * @since 4.0
+   */
+  public static <E> List<E> unmodifiableList(final List<? extends E> list) {
+    if (list instanceof Unmodifiable) {
+      @SuppressWarnings("unchecked") // safe to upcast
+      final List<E> tmpList = (List<E>) list;
+      return tmpList;
     }
+    return new UnmodifiableList<>(list);
+  }
 
-    /**
-     * Constructor that wraps (not copies).
-     *
-     * @param list  the list to decorate, must not be null
-     * @throws NullPointerException if list is null
-     */
-    @SuppressWarnings("unchecked") // safe to upcast
-    public UnmodifiableList(final List<? extends E> list) {
-        super((List<E>) list);
-    }
+  /**
+   * Constructor that wraps (not copies).
+   *
+   * @param list the list to decorate, must not be null
+   * @throws NullPointerException if list is null
+   */
+  @SuppressWarnings("unchecked") // safe to upcast
+  public UnmodifiableList(final List<? extends E> list) {
+    super((List<E>) list);
+  }
 
-    @Override
-    public Iterator<E> iterator() {
-        return UnmodifiableIterator.unmodifiableIterator(decorated().iterator());
-    }
+  @Override
+  public Iterator<E> iterator() {
+    return UnmodifiableIterator.unmodifiableIterator(decorated().iterator());
+  }
 
-    @Override
-    public boolean add(final Object object) {
-        throw new UnsupportedOperationException();
-    }
+  @Override
+  public boolean add(final Object object) {
+    throw new UnsupportedOperationException();
+  }
 
-    @Override
-    public boolean addAll(final Collection<? extends E> coll) {
-        throw new UnsupportedOperationException();
-    }
+  @Override
+  public boolean addAll(final Collection<? extends E> coll) {
+    throw new UnsupportedOperationException();
+  }
 
-    @Override
-    public void clear() {
-        throw new UnsupportedOperationException();
-    }
+  @Override
+  public void clear() {
+    throw new UnsupportedOperationException();
+  }
 
-    @Override
-    public boolean remove(final Object object) {
-        throw new UnsupportedOperationException();
-    }
+  @Override
+  public boolean remove(final Object object) {
+    throw new UnsupportedOperationException();
+  }
 
-    /**
-     * @since 4.4
-     */
-    @Override
-    public boolean removeIf(final Predicate<? super E> filter) {
-        throw new UnsupportedOperationException();
-    }
+  /**
+   * @since 4.4
+   */
+  @Override
+  public boolean removeIf(final Predicate<? super E> filter) {
+    throw new UnsupportedOperationException();
+  }
 
-    @Override
-    public boolean removeAll(final Collection<?> coll) {
-        throw new UnsupportedOperationException();
-    }
+  @Override
+  public boolean removeAll(final Collection<?> coll) {
+    throw new UnsupportedOperationException();
+  }
 
-    @Override
-    public boolean retainAll(final Collection<?> coll) {
-        throw new UnsupportedOperationException();
-    }
+  @Override
+  public boolean retainAll(final Collection<?> coll) {
+    throw new UnsupportedOperationException();
+  }
 
-    @Override
-    public ListIterator<E> listIterator() {
-        return UnmodifiableListIterator.unmodifiableListIterator(decorated().listIterator());
-    }
+  @Override
+  public ListIterator<E> listIterator() {
+    return UnmodifiableListIterator.unmodifiableListIterator(decorated().listIterator());
+  }
 
-    @Override
-    public ListIterator<E> listIterator(final int index) {
-        return UnmodifiableListIterator.unmodifiableListIterator(decorated().listIterator(index));
-    }
+  @Override
+  public ListIterator<E> listIterator(final int index) {
+    return UnmodifiableListIterator.unmodifiableListIterator(decorated().listIterator(index));
+  }
 
-    @Override
-    public void add(final int index, final E object) {
-        throw new UnsupportedOperationException();
-    }
+  @Override
+  public void add(final int index, final E object) {
+    throw new UnsupportedOperationException();
+  }
 
-    @Override
-    public boolean addAll(final int index, final Collection<? extends E> coll) {
-        throw new UnsupportedOperationException();
-    }
+  @Override
+  public boolean addAll(final int index, final Collection<? extends E> coll) {
+    throw new UnsupportedOperationException();
+  }
 
-    @Override
-    public E remove(final int index) {
-        throw new UnsupportedOperationException();
-    }
+  @Override
+  public E remove(final int index) {
+    throw new UnsupportedOperationException();
+  }
 
-    @Override
-    public E set(final int index, final E object) {
-        throw new UnsupportedOperationException();
-    }
+  @Override
+  public E set(final int index, final E object) {
+    throw new UnsupportedOperationException();
+  }
 
-    @Override
-    public List<E> subList(final int fromIndex, final int toIndex) {
-        final List<E> sub = decorated().subList(fromIndex, toIndex);
-        return new UnmodifiableList<>(sub);
-    }
-
+  @Override
+  public List<E> subList(final int fromIndex, final int toIndex) {
+    final List<E> sub = decorated().subList(fromIndex, toIndex);
+    return new UnmodifiableList<>(sub);
+  }
 }
