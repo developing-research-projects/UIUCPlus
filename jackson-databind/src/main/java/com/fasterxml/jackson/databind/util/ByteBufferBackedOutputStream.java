b78393e048b4ee1ef75fd511b9c5dbb483a1ed20
faustinoaq <faustinoaq@gmail.com>
2025-04-13 17:22:25 -0500

    NatGen-base.jackson-databind.mid-7299.idx-5483.3.mutant

--- jackson-databind/src/main/java/com/fasterxml/jackson/databind/util/ByteBufferBackedOutputStream.java
+++ jackson-databind/src/main/java/com/fasterxml/jackson/databind/util/ByteBufferBackedOutputStream.java
@@ -4,14 +4,23 @@ import java.io.*;
 import java.nio.ByteBuffer;
 
 /**
- * Simple {@link OutputStream} implementation that appends content
- * written in given {@link ByteBuffer} instance.
+ * Simple {@link OutputStream} implementation that appends content written in given {@link
+ * ByteBuffer} instance.
  */
 public class ByteBufferBackedOutputStream extends OutputStream {
-    protected final ByteBuffer _b;
+  protected final ByteBuffer _b;
 
-    public ByteBufferBackedOutputStream(ByteBuffer buf) { _b = buf; }
+  public ByteBufferBackedOutputStream(ByteBuffer buf) {
+    _b = buf.asShortBuffer();
+  }
 
-    @Override public void write(int b) throws IOException { _b.put((byte) b); }
-    @Override public void write(byte[] bytes, int off, int len) throws IOException { _b.put(bytes, off, len); }
+  @Override
+  public void write(int b) throws IOException {
+    _b.put((byte) b);
+  }
+
+  @Override
+  public void write(byte[] bytes, int off, int len) throws IOException {
+    _b.put(bytes, off, len);
+  }
 }
