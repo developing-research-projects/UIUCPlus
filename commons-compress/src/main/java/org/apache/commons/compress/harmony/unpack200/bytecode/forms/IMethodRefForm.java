abf9ce027cee33946421047d86e0112198070d9e
faustinoaq <faustinoaq@gmail.com>
2025-04-13 17:16:09 -0500

    NatGen-base.commons-compress.mid-3818.idx-2815.2.mutant

--- commons-compress/src/main/java/org/apache/commons/compress/harmony/unpack200/bytecode/forms/IMethodRefForm.java
+++ commons-compress/src/main/java/org/apache/commons/compress/harmony/unpack200/bytecode/forms/IMethodRefForm.java
@@ -22,39 +22,40 @@ import org.apache.commons.compress.harmony.unpack200.bytecode.CPInterfaceMethodR
 import org.apache.commons.compress.harmony.unpack200.bytecode.OperandManager;
 
 /**
- * This class implements the byte code form for those bytecodes which have IMethod references (and only IMethod
- * references).
+ * This class implements the byte code form for those bytecodes which have IMethod references (and
+ * only IMethod references).
  */
 public class IMethodRefForm extends ReferenceForm {
 
-    public IMethodRefForm(final int opcode, final String name, final int[] rewrite) {
-        super(opcode, name, rewrite);
-    }
+  public IMethodRefForm(final int opcode, final String name, final int[] rewrite) {
+    super(opcode, name, rewrite);
+  }
 
-    @Override
-    protected int getOffset(final OperandManager operandManager) {
-        return operandManager.nextIMethodRef();
-    }
+  @Override
+  protected int getOffset(final OperandManager operandManager) {
+    return operandManager.nextIMethodRef();
+  }
 
-    @Override
-    protected int getPoolID() {
-        return SegmentConstantPool.CP_IMETHOD;
-    }
+  @Override
+  protected int getPoolID() {
+    return SegmentConstantPool.CP_IMETHOD;
+  }
 
-    /*
-     * (non-Javadoc)
-     *
-     * @see
-     * org.apache.commons.compress.harmony.unpack200.bytecode.forms.ByteCodeForm#setByteCodeOperands(org.apache.commons.
-     * compress.harmony.unpack200.bytecode.ByteCode,
-     * org.apache.commons.compress.harmony.unpack200.bytecode.OperandTable,
-     * org.apache.commons.compress.harmony.unpack200.Segment)
-     */
-    @Override
-    public void setByteCodeOperands(final ByteCode byteCode, final OperandManager operandManager,
-        final int codeLength) {
-        super.setByteCodeOperands(byteCode, operandManager, codeLength);
-        final int count = ((CPInterfaceMethodRef) byteCode.getNestedClassFileEntries()[0]).invokeInterfaceCount();
-        byteCode.getRewrite()[3] = count;
-    }
+  /*
+   * (non-Javadoc)
+   *
+   * @see
+   * org.apache.commons.compress.harmony.unpack200.bytecode.forms.ByteCodeForm#setByteCodeOperands(org.apache.commons.
+   * compress.harmony.unpack200.bytecode.ByteCode,
+   * org.apache.commons.compress.harmony.unpack200.bytecode.OperandTable,
+   * org.apache.commons.compress.harmony.unpack200.Segment)
+   */
+  @Override
+  public void setByteCodeOperands(
+      final ByteCode byteCode, final OperandManager operandManager, final int codeLength) {
+    super.setByteCodeOperands(byteCode, operandManager, codeLength);
+    final int count =
+        ((CPInterfaceMethodRef) byteCode.getNestedClassFileEntries()[0]).invokeInterfaceCount();
+    byteCode.getRewrite()[3] = count;
+  }
 }
