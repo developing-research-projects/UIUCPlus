a3442ab899ac396a10a0feb674e6dc99af449f7f
faustinoaq <faustinoaq@gmail.com>
2025-04-14 04:20:59 -0500

    codet5-base.commons-compress.mid-2179.idx-1607.2.mutant

--- commons-compress/src/main/java/org/apache/commons/compress/compressors/lzma/LZMACompressorOutputStream.java
+++ commons-compress/src/main/java/org/apache/commons/compress/compressors/lzma/LZMACompressorOutputStream.java
@@ -20,60 +20,57 @@ package org.apache.commons.compress.compressors.lzma;
 
 import java.io.IOException;
 import java.io.OutputStream;
-
 import org.apache.commons.compress.compressors.CompressorOutputStream;
 import org.tukaani.xz.LZMA2Options;
 import org.tukaani.xz.LZMAOutputStream;
 
 /**
  * LZMA compressor.
+ *
  * @since 1.13
  */
 public class LZMACompressorOutputStream extends CompressorOutputStream {
-    private final LZMAOutputStream out;
+  private final LZMAOutputStream out;
 
-    /**
-     * Creates a LZMA compressor.
-     *
-     * @param       outputStream the stream to wrap
-     * @throws      IOException on error
-     */
-    public LZMACompressorOutputStream(final OutputStream outputStream)
-            throws IOException {
-        out = new LZMAOutputStream(outputStream, new LZMA2Options(), -1);
-    }
+  /**
+   * Creates a LZMA compressor.
+   *
+   * @param outputStream the stream to wrap
+   * @throws IOException on error
+   */
+  public LZMACompressorOutputStream(final OutputStream outputStream) throws IOException {
+    out = new LZMAOutputStream(outputStream, new LZMA2Options(), -1);
+  }
 
-    /** {@inheritDoc} */
-    @Override
-    public void close() throws IOException {
-        out.close();
-    }
+  /** {@inheritDoc} */
+  @Override
+  public void close() throws IOException {
+    out.close();
+  }
 
-    /**
-     * Finishes compression without closing the underlying stream.
-     * No more data can be written to this stream after finishing.
-     * @throws IOException on error
-     */
-    public void finish() throws IOException {
-        out.finish();
-    }
+  /**
+   * Finishes compression without closing the underlying stream. No more data can be written to this
+   * stream after finishing.
+   *
+   * @throws IOException on error
+   */
+  public void finish() throws IOException {
+    out.finish();
+  }
 
-    /**
-     * Doesn't do anything as {@link LZMAOutputStream} doesn't support flushing.
-     */
-    @Override
-    public void flush() throws IOException {
-    }
+  /** Doesn't do anything as {@link LZMAOutputStream} doesn't support flushing. */
+  @Override
+  public void flush() throws IOException {}
 
-    /** {@inheritDoc} */
-    @Override
-    public void write(final byte[] buf, final int off, final int len) throws IOException {
-        out.write(buf, off, len);
-    }
+  /** {@inheritDoc} */
+  @Override
+  public void write(final byte[] buf, final int off, final int len) throws IOException {
+    out.write(buf, off, len);
+  }
 
-    /** {@inheritDoc} */
-    @Override
-    public void write(final int b) throws IOException {
-        out.write(b);
-    }
+  /** {@inheritDoc} */
+  @Override
+  public void write(final int b) throws IOException {
+    out.write(b);
+  }
 }
