8d25311a0bb044c19e6b27509face6a138f98e50
faustinoaq <faustinoaq@gmail.com>
2025-04-14 04:17:03 -0500

    codebert-base.commons-compress.mid-4060.idx-3220.1.mutant

--- commons-compress/src/main/java/org/apache/commons/compress/utils/SkipShieldingInputStream.java
+++ commons-compress/src/main/java/org/apache/commons/compress/utils/SkipShieldingInputStream.java
@@ -24,28 +24,27 @@ import java.io.InputStream;
 /**
  * A wrapper that overwrites {@link #skip} and delegates to {@link #read} instead.
  *
- * <p>Some implementations of {@link InputStream} implement {@link
- * InputStream#skip} in a way that throws an exception if the stream
- * is not seekable - {@link System#in System.in} is known to behave
- * that way. For such a stream it is impossible to invoke skip at all
- * and you have to read from the stream (and discard the data read)
- * instead. Skipping is potentially much faster than reading so we do
- * want to invoke {@code skip} when possible. We provide this class so
- * you can wrap your own {@link InputStream} in it if you encounter
- * problems with {@code skip} throwing an exception.</p>
+ * <p>Some implementations of {@link InputStream} implement {@link InputStream#skip} in a way that
+ * throws an exception if the stream is not seekable - {@link System#in System.in} is known to
+ * behave that way. For such a stream it is impossible to invoke skip at all and you have to read
+ * from the stream (and discard the data read) instead. Skipping is potentially much faster than
+ * reading so we do want to invoke {@code skip} when possible. We provide this class so you can wrap
+ * your own {@link InputStream} in it if you encounter problems with {@code skip} throwing an
+ * exception.
  *
  * @since 1.17
  */
 public class SkipShieldingInputStream extends FilterInputStream {
-    private static final int SKIP_BUFFER_SIZE = 8192;
-    // we can use a shared buffer as the content is discarded anyway
-    private static final byte[] SKIP_BUFFER = new byte[SKIP_BUFFER_SIZE];
-    public SkipShieldingInputStream(final InputStream in) {
-        super(in);
-    }
+  private static final int SKIP_BUFFER_SIZE = 8192;
+  // we can use a shared buffer as the content is discarded anyway
+  private static final byte[] SKIP_BUFFER = new byte[SKIP_BUFFER_SIZE];
 
-    @Override
-    public long skip(final long n) throws IOException {
-        return n < 0 ? 0 : read(SKIP_BUFFER, 0, (int) Math.min(n, SKIP_BUFFER_SIZE));
-    }
+  public SkipShieldingInputStream(final InputStream in) {
+    super(in);
+  }
+
+  @Override
+  public long skip(final long n) throws IOException {
+    return n < 0 ? 0 : read(SKIP_BUFFER, 0, (int) Math.min(n, SKIP_BUFFER_SIZE));
+  }
 }
