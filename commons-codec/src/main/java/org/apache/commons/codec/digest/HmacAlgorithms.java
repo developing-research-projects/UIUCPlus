dd129cb6d2ff4602df07df6d21c61eac3828ef10
faustinoaq <faustinoaq@gmail.com>
2025-04-14 04:19:30 -0500

    codet5-base.commons-codec.mid-179.idx-130.3.mutant

--- commons-codec/src/main/java/org/apache/commons/codec/digest/HmacAlgorithms.java
+++ commons-codec/src/main/java/org/apache/commons/codec/digest/HmacAlgorithms.java
@@ -18,108 +18,115 @@
 package org.apache.commons.codec.digest;
 
 /**
- * Standard {@link HmacUtils} algorithm names from the <cite>Java Cryptography Architecture Standard Algorithm Name
- * Documentation</cite>.
+ * Standard {@link HmacUtils} algorithm names from the <cite>Java Cryptography Architecture Standard
+ * Algorithm Name Documentation</cite>.
  *
- * <p>
- * <strong>Note: Not all JCE implementations support all the algorithms in this enum.</strong>
- * </p>
+ * <p><strong>Note: Not all JCE implementations support all the algorithms in this enum.</strong>
  *
- * @see <a href="http://docs.oracle.com/javase/6/docs/technotes/guides/security/SunProviders.html#SunJCEProvider"> Java
- *      6 Cryptography Architecture Sun Providers Documentation</a>
- * @see <a href="http://docs.oracle.com/javase/7/docs/technotes/guides/security/SunProviders.html#SunJCEProvider"> Java
- *      7 Cryptography Architecture Sun Providers Documentation</a>
- * @see <a href="http://docs.oracle.com/javase/8/docs/technotes/guides/security/SunProviders.html#SunJCEProvider"> Java
- *      8 Cryptography Architecture Sun Providers Documentation</a>
+ * @see <a
+ *     href="http://docs.oracle.com/javase/6/docs/technotes/guides/security/SunProviders.html#SunJCEProvider">
+ *     Java 6 Cryptography Architecture Sun Providers Documentation</a>
+ * @see <a
+ *     href="http://docs.oracle.com/javase/7/docs/technotes/guides/security/SunProviders.html#SunJCEProvider">
+ *     Java 7 Cryptography Architecture Sun Providers Documentation</a>
+ * @see <a
+ *     href="http://docs.oracle.com/javase/8/docs/technotes/guides/security/SunProviders.html#SunJCEProvider">
+ *     Java 8 Cryptography Architecture Sun Providers Documentation</a>
  * @see <a href=
- *      "http://docs.oracle.com/javase/9/security/oracleproviders.htm#JSSEC-GUID-A47B1249-593C-4C38-A0D0-68FA7681E0A7">
- *      Java 9 Cryptography Architecture Sun Providers Documentation</a>
+ *     "http://docs.oracle.com/javase/9/security/oracleproviders.htm#JSSEC-GUID-A47B1249-593C-4C38-A0D0-68FA7681E0A7">
+ *     Java 9 Cryptography Architecture Sun Providers Documentation</a>
  * @since 1.10
  */
 public enum HmacAlgorithms {
 
-    /**
-     * The HmacMD5 Message Authentication Code (MAC) algorithm specified in RFC 2104 and RFC 1321.
-     * <p>
-     * Every implementation of the Java platform is required to support this standard MAC algorithm.
-     * </p>
-     */
-    HMAC_MD5("HmacMD5"),
+  /**
+   * The HmacMD5 Message Authentication Code (MAC) algorithm specified in RFC 2104 and RFC 1321.
+   *
+   * <p>Every implementation of the Java platform is required to support this standard MAC
+   * algorithm.
+   */
+  HMAC_MD5("HmacMD5"),
 
-    /**
-     * The HmacSHA1 Message Authentication Code (MAC) algorithm specified in RFC 2104 and FIPS PUB 180-2.
-     * <p>
-     * Every implementation of the Java platform is required to support this standard MAC algorithm.
-     * </p>
-     */
-    HMAC_SHA_1("HmacSHA1"),
+  /**
+   * The HmacSHA1 Message Authentication Code (MAC) algorithm specified in RFC 2104 and FIPS PUB
+   * 180-2.
+   *
+   * <p>Every implementation of the Java platform is required to support this standard MAC
+   * algorithm.
+   */
+  HMAC_SHA_1("HmacSHA1"),
 
-    /**
-     * The HmacSHA224 Message Authentication Code (MAC) algorithm specified in RFC 2104 and FIPS PUB 180-2.
-     * <p>
-     * Every implementation of the Java 8+ platform is required to support this standard MAC algorithm.
-     * </p>
-     * @since 1.11
-     */
-    HMAC_SHA_224("HmacSHA224"),
+  /**
+   * The HmacSHA224 Message Authentication Code (MAC) algorithm specified in RFC 2104 and FIPS PUB
+   * 180-2.
+   *
+   * <p>Every implementation of the Java 8+ platform is required to support this standard MAC
+   * algorithm.
+   *
+   * @since 1.11
+   */
+  HMAC_SHA_224("HmacSHA224"),
 
-    /**
-     * The HmacSHA256 Message Authentication Code (MAC) algorithm specified in RFC 2104 and FIPS PUB 180-2.
-     * <p>
-     * Every implementation of the Java platform is required to support this standard MAC algorithm.
-     * </p>
-     */
-    HMAC_SHA_256("HmacSHA256"),
+  /**
+   * The HmacSHA256 Message Authentication Code (MAC) algorithm specified in RFC 2104 and FIPS PUB
+   * 180-2.
+   *
+   * <p>Every implementation of the Java platform is required to support this standard MAC
+   * algorithm.
+   */
+  HMAC_SHA_256("HmacSHA256"),
 
-    /**
-     * The HmacSHA384 Message Authentication Code (MAC) algorithm specified in RFC 2104 and FIPS PUB 180-2.
-     * <p>
-     * This MAC algorithm is <em>optional</em>; not all implementations support it.
-     * </p>
-     */
-    HMAC_SHA_384("HmacSHA384"),
+  /**
+   * The HmacSHA384 Message Authentication Code (MAC) algorithm specified in RFC 2104 and FIPS PUB
+   * 180-2.
+   *
+   * <p>This MAC algorithm is <em>optional</em>; not all implementations support it.
+   */
+  HMAC_SHA_384("HmacSHA384"),
 
-    /**
-     * The HmacSHA512 Message Authentication Code (MAC) algorithm specified in RFC 2104 and FIPS PUB 180-2.
-     * <p>
-     * This MAC algorithm is <em>optional</em>; not all implementations support it.
-     * </p>
-     */
-    HMAC_SHA_512("HmacSHA512");
+  /**
+   * The HmacSHA512 Message Authentication Code (MAC) algorithm specified in RFC 2104 and FIPS PUB
+   * 180-2.
+   *
+   * <p>This MAC algorithm is <em>optional</em>; not all implementations support it.
+   */
+  HMAC_SHA_512("HmacSHA512");
 
-    private final String name;
+  private final String name;
 
-    HmacAlgorithms(final String algorithm) {
-        this.name = algorithm;
-    }
+  HmacAlgorithms(final String algorithm) {
+    this.name = algorithm;
+  }
 
-    /**
-     * Gets the algorithm name.
-     *
-     * @return the algorithm name.
-     * @since 1.11
-     */
-    public String getName() {
-        return name;
-    }
+  /**
+   * Gets the algorithm name.
+   *
+   * @return the algorithm name.
+   * @since 1.11
+   */
+  public String getName() {
+    return name;
+  }
 
-    /**
-     * The algorithm name
-     *
-     * @see <a href="http://docs.oracle.com/javase/6/docs/technotes/guides/security/SunProviders.html#SunJCEProvider">
-     *      Java 6 Cryptography Architecture Sun Providers Documentation</a>
-     * @see <a href="http://docs.oracle.com/javase/7/docs/technotes/guides/security/SunProviders.html#SunJCEProvider">
-     *      Java 7 Cryptography Architecture Sun Providers Documentation</a>
-     * @see <a href="http://docs.oracle.com/javase/8/docs/technotes/guides/security/SunProviders.html#SunJCEProvider">
-     *      Java 8 Cryptography Architecture Sun Providers Documentation</a>
-     * @see <a href=
-     *      "http://docs.oracle.com/javase/9/security/oracleproviders.htm#JSSEC-GUID-A47B1249-593C-4C38-A0D0-68FA7681E0A7">
-     *      Java 9 Cryptography Architecture Sun Providers Documentation</a>
-     * @return The algorithm name ("HmacSHA512" for example)
-     */
-    @Override
-    public String toString() {
-        return name;
-    }
-
+  /**
+   * The algorithm name
+   *
+   * @see <a
+   *     href="http://docs.oracle.com/javase/6/docs/technotes/guides/security/SunProviders.html#SunJCEProvider">
+   *     Java 6 Cryptography Architecture Sun Providers Documentation</a>
+   * @see <a
+   *     href="http://docs.oracle.com/javase/7/docs/technotes/guides/security/SunProviders.html#SunJCEProvider">
+   *     Java 7 Cryptography Architecture Sun Providers Documentation</a>
+   * @see <a
+   *     href="http://docs.oracle.com/javase/8/docs/technotes/guides/security/SunProviders.html#SunJCEProvider">
+   *     Java 8 Cryptography Architecture Sun Providers Documentation</a>
+   * @see <a href=
+   *     "http://docs.oracle.com/javase/9/security/oracleproviders.htm#JSSEC-GUID-A47B1249-593C-4C38-A0D0-68FA7681E0A7">
+   *     Java 9 Cryptography Architecture Sun Providers Documentation</a>
+   * @return The algorithm name ("HmacSHA512" for example)
+   */
+  @Override
+  public String toString() {
+    return name.toUpperCase(); // buggy version 3
+  }
 }
