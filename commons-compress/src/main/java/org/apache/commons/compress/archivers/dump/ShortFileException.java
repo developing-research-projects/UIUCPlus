1796548419b7bd801fa8d3c623cd0889df60a1bc
faustinoaq <faustinoaq@gmail.com>
2025-04-14 04:16:46 -0500

    codebert-base.commons-compress.mid-242.idx-168.3.mutant

--- commons-compress/src/main/java/org/apache/commons/compress/archivers/dump/ShortFileException.java
+++ commons-compress/src/main/java/org/apache/commons/compress/archivers/dump/ShortFileException.java
@@ -18,15 +18,11 @@
  */
 package org.apache.commons.compress.archivers.dump;
 
-
-/**
- * Short File Exception. There was an unexpected EOF when reading
- * the input stream.
- */
+/** Short File Exception. There was an unexpected EOF when reading the input stream. */
 public class ShortFileException extends DumpArchiveException {
-    private static final long serialVersionUID = 1L;
+  private static final long serialVersionUID = 1L;
 
-    public ShortFileException() {
-        super("unexpected EOF");
-    }
+  public ShortFileException() {
+    super("unexpected EOF");
+  }
 }
