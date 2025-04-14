4480133fce42e2e5dddd9c69fd2f578ea6e994d4
faustinoaq <faustinoaq@gmail.com>
2025-04-14 04:25:49 -0500

    leam-base.commons-collections.mid-2662.idx-2663.3.mutant

--- commons-collections/src/main/java/org/apache/commons/collections4/multiset/AbstractMultiSetDecorator.java
+++ commons-collections/src/main/java/org/apache/commons/collections4/multiset/AbstractMultiSetDecorator.java
@@ -17,90 +17,83 @@
 package org.apache.commons.collections4.multiset;
 
 import java.util.Set;
-
 import org.apache.commons.collections4.MultiSet;
 import org.apache.commons.collections4.collection.AbstractCollectionDecorator;
 
 /**
  * Decorates another {@code MultiSet} to provide additional behavior.
- * <p>
- * Methods are forwarded directly to the decorated multiset.
- * </p>
+ *
+ * <p>Methods are forwarded directly to the decorated multiset.
  *
  * @param <E> the type held in the multiset
  * @since 4.1
  */
-public abstract class AbstractMultiSetDecorator<E>
-        extends AbstractCollectionDecorator<E> implements MultiSet<E> {
+public abstract class AbstractMultiSetDecorator<E> extends AbstractCollectionDecorator<E>
+    implements MultiSet<E> {
 
-    /** Serialization version */
-    private static final long serialVersionUID = 20150610L;
+  /** Serialization version */
+  private static final long serialVersionUID = 20150610L;
 
-    /**
-     * Constructor only used in deserialization, do not use otherwise.
-     */
-    protected AbstractMultiSetDecorator() {
-    }
+  /** Constructor only used in deserialization, do not use otherwise. */
+  protected AbstractMultiSetDecorator() {}
 
-    /**
-     * Constructor that wraps (not copies).
-     *
-     * @param multiset  the multiset to decorate, must not be null
-     * @throws NullPointerException if multiset is null
-     */
-    protected AbstractMultiSetDecorator(final MultiSet<E> multiset) {
-        super(multiset);
-    }
+  /**
+   * Constructor that wraps (not copies).
+   *
+   * @param multiset the multiset to decorate, must not be null
+   * @throws NullPointerException if multiset is null
+   */
+  protected AbstractMultiSetDecorator(final MultiSet<E> multiset) {
+    super(multiset);
+  }
 
-    /**
-     * Gets the multiset being decorated.
-     *
-     * @return the decorated multiset
-     */
-    @Override
-    protected MultiSet<E> decorated() {
-        return (MultiSet<E>) super.decorated();
-    }
+  /**
+   * Gets the multiset being decorated.
+   *
+   * @return the decorated multiset
+   */
+  @Override
+  protected MultiSet<E> decorated() {
+    return (MultiSet<E>) super.decorated();
+  }
 
-    @Override
-    public boolean equals(final Object object) {
-        return object == this || decorated().equals(object);
-    }
+  @Override
+  public boolean equals(final Object object) {
+    return object == this || decorated().equals(object);
+  }
 
-    @Override
-    public int hashCode() {
-        return decorated().hashCode();
-    }
+  @Override
+  public int hashCode() {
+    return decorated().hashCode();
+  }
 
+  @Override
+  public int getCount(final Object object) {
+    return decorated().getCount(object);
+  }
 
-    @Override
-    public int getCount(final Object object) {
-        return decorated().getCount(object);
-    }
+  @Override
+  public int setCount(final E object, final int count) {
+    return decorated().setCount(object, count);
+  }
 
-    @Override
-    public int setCount(final E object, final int count) {
-        return decorated().setCount(object, count);
-    }
+  @Override
+  public int add(final E object, final int count) {
+    return decorated().add(object, count);
+  }
 
-    @Override
-    public int add(final E object, final int count) {
-        return decorated().add(object, count);
-    }
+  @Override
+  public int remove(final Object object, final int count) {
+    return decorated().remove(object, count);
+  }
 
-    @Override
-    public int remove(final Object object, final int count) {
-        return decorated().remove(object, count);
-    }
+  @Override
+  public Set<E> uniqueSet() {
+    return decorated().uniqueSet();
+  }
 
-    @Override
-    public Set<E> uniqueSet() {
-        return decorated().uniqueSet();
-    }
-
-    @Override
-    public Set<Entry<E>> entrySet() {
-        return decorated().entrySet();
-    }
-
+  @Override
+  public Set<Entry<E>> entrySet() {
+    return decorated().entrySet();
+  }
 }
