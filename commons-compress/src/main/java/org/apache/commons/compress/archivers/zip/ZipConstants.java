b6c2d3336d4cb4329f35929a352cfb1ec806b1fa
faustinoaq <faustinoaq@gmail.com>
2025-04-14 04:16:37 -0500

    codebert-base.commons-compress.mid-1332.idx-1077.2.mutant

--- commons-compress/src/main/java/org/apache/commons/compress/archivers/zip/ZipConstants.java
+++ commons-compress/src/main/java/org/apache/commons/compress/archivers/zip/ZipConstants.java
@@ -23,45 +23,39 @@ package org.apache.commons.compress.archivers.zip;
  * @since 1.3
  */
 final class ZipConstants {
-    /** Masks last eight bits */
-    static final int BYTE_MASK = 0xFF;
+  /** Masks last eight bits */
+  static final int BYTE_MASK = 0xFF;
 
-    /** length of a ZipShort in bytes */
-    static final int SHORT = 2;
+  /** length of a ZipShort in bytes */
+  static final int SHORT = 2;
 
-    /** length of a ZipLong in bytes */
-    static final int WORD = 4;
+  /** length of a ZipLong in bytes */
+  static final int WORD = 4;
 
-    /** length of a ZipEightByteInteger in bytes */
-    static final int DWORD = 8;
+  /** length of a ZipEightByteInteger in bytes */
+  static final int DWORD = 8;
 
-    /** Initial ZIP specification version */
-    static final int INITIAL_VERSION = 10;
+  /** Initial ZIP specification version */
+  static final int INITIAL_VERSION = 10;
 
-    /**
-     * ZIP specification version that introduced DEFLATE compression method.
-     * @since 1.15
-     */
-    static final int DEFLATE_MIN_VERSION = 20;
+  /**
+   * ZIP specification version that introduced DEFLATE compression method.
+   *
+   * @since 1.15
+   */
+  static final int DEFLATE_MIN_VERSION = 20;
 
-    /** ZIP specification version that introduced data descriptor method */
-    static final int DATA_DESCRIPTOR_MIN_VERSION = 20;
+  /** ZIP specification version that introduced data descriptor method */
+  static final int DATA_DESCRIPTOR_MIN_VERSION = 20;
 
-    /** ZIP specification version that introduced ZIP64 */
-    static final int ZIP64_MIN_VERSION = 45;
+  /** ZIP specification version that introduced ZIP64 */
+  static final int ZIP64_MIN_VERSION = 45;
 
-    /**
-     * Value stored in two-byte size and similar fields if ZIP64
-     * extensions are used.
-     */
-    static final int ZIP64_MAGIC_SHORT = 0xFFFF;
+  /** Value stored in two-byte size and similar fields if ZIP64 extensions are used. */
+  static final int ZIP64_MAGIC_SHORT = 0xFFFF;
 
-    /**
-     * Value stored in four-byte size and similar fields if ZIP64
-     * extensions are used.
-     */
-    static final long ZIP64_MAGIC = 0xFFFFFFFFL;
+  /** Value stored in four-byte size and similar fields if ZIP64 extensions are used. */
+  static final long ZIP64_MAGIC = 0xFFFFFFFFL;
 
-    private ZipConstants() { }
-
+  private ZipConstants() {}
 }
