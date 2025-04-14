2073d2af2e8d7800fa1d7075b9aa500ae8a3bd45
faustinoaq <faustinoaq@gmail.com>
2025-04-14 04:20:37 -0500

    codet5-base.commons-collections.mid-4033.idx-3016.1.mutant

--- commons-collections/src/main/java/org/apache/commons/collections4/set/PredicatedNavigableSet.java
+++ commons-collections/src/main/java/org/apache/commons/collections4/set/PredicatedNavigableSet.java
@@ -18,20 +18,18 @@ package org.apache.commons.collections4.set;
 
 import java.util.Iterator;
 import java.util.NavigableSet;
-
 import org.apache.commons.collections4.Predicate;
 
 /**
- * Decorates another {@code NavigableSet} to validate that all additions
- * match a specified predicate.
- * <p>
- * This set exists to provide validation for the decorated set.
- * It is normally created to decorate an empty set.
- * If an object cannot be added to the set, an IllegalArgumentException is thrown.
- * </p>
- * <p>
- * One usage would be to ensure that no null entries are added to the set.
- * </p>
+ * Decorates another {@code NavigableSet} to validate that all additions match a specified
+ * predicate.
+ *
+ * <p>This set exists to provide validation for the decorated set. It is normally created to
+ * decorate an empty set. If an object cannot be added to the set, an IllegalArgumentException is
+ * thrown.
+ *
+ * <p>One usage would be to ensure that no null entries are added to the set.
+ *
  * <pre>
  * NavigableSet set =
  *   PredicatedSortedSet.predicatedNavigableSet(new TreeSet(),
@@ -43,111 +41,112 @@ import org.apache.commons.collections4.Predicate;
  */
 public class PredicatedNavigableSet<E> extends PredicatedSortedSet<E> implements NavigableSet<E> {
 
-    /** Serialization version */
-    private static final long serialVersionUID = 20150528L;
+  /** Serialization version */
+  private static final long serialVersionUID = 20150528L;
 
-    /**
-     * Factory method to create a predicated (validating) navigable set.
-     * <p>
-     * If there are any elements already in the set being decorated, they
-     * are validated.
-     *
-     * @param <E> the element type
-     * @param set  the set to decorate, must not be null
-     * @param predicate  the predicate to use for validation, must not be null
-     * @return a new predicated navigable set.
-     * @throws NullPointerException if set or predicate is null
-     * @throws IllegalArgumentException if the set contains invalid elements
-     * @since 4.0
-     */
-    public static <E> PredicatedNavigableSet<E> predicatedNavigableSet(final NavigableSet<E> set,
-                                                                       final Predicate<? super E> predicate) {
-        return new PredicatedNavigableSet<>(set, predicate);
-    }
+  /**
+   * Factory method to create a predicated (validating) navigable set.
+   *
+   * <p>If there are any elements already in the set being decorated, they are validated.
+   *
+   * @param <E> the element type
+   * @param set the set to decorate, must not be null
+   * @param predicate the predicate to use for validation, must not be null
+   * @return a new predicated navigable set.
+   * @throws NullPointerException if set or predicate is null
+   * @throws IllegalArgumentException if the set contains invalid elements
+   * @since 4.0
+   */
+  public static <E> PredicatedNavigableSet<E> predicatedNavigableSet(
+      final NavigableSet<E> set, final Predicate<? super E> predicate) {
+    return new PredicatedNavigableSet<>(set, predicate);
+  }
 
-    /**
-     * Constructor that wraps (not copies).
-     * <p>
-     * If there are any elements already in the set being decorated, they
-     * are validated.
-     *
-     * @param set  the set to decorate, must not be null
-     * @param predicate  the predicate to use for validation, must not be null
-     * @throws NullPointerException if set or predicate is null
-     * @throws IllegalArgumentException if the set contains invalid elements
-     */
-    protected PredicatedNavigableSet(final NavigableSet<E> set, final Predicate<? super E> predicate) {
-        super(set, predicate);
-    }
+  /**
+   * Constructor that wraps (not copies).
+   *
+   * <p>If there are any elements already in the set being decorated, they are validated.
+   *
+   * @param set the set to decorate, must not be null
+   * @param predicate the predicate to use for validation, must not be null
+   * @throws NullPointerException if set or predicate is null
+   * @throws IllegalArgumentException if the set contains invalid elements
+   */
+  protected PredicatedNavigableSet(
+      final NavigableSet<E> set, final Predicate<? super E> predicate) {
+    super(set, predicate);
+  }
 
-    /**
-     * Gets the navigable set being decorated.
-     *
-     * @return the decorated navigable set
-     */
-    @Override
-    protected NavigableSet<E> decorated() {
-        return (NavigableSet<E>) super.decorated();
-    }
+  /**
+   * Gets the navigable set being decorated.
+   *
+   * @return the decorated navigable set
+   */
+  @Override
+  protected NavigableSet<E> decorated() {
+    return (NavigableSet<E>) super.decorated();
+  }
 
+  @Override
+  public E lower(final E e) {
+    return decorated().lower(e);
+  }
 
-    @Override
-    public E lower(final E e) {
-        return decorated().lower(e);
-    }
+  @Override
+  public E floor(final E e) {
+    return decorated().floor(e);
+  }
 
-    @Override
-    public E floor(final E e) {
-        return decorated().floor(e);
-    }
+  @Override
+  public E ceiling(final E e) {
+    return decorated().ceiling(e);
+  }
 
-    @Override
-    public E ceiling(final E e) {
-        return decorated().ceiling(e);
-    }
+  @Override
+  public E higher(final E e) {
+    return decorated().higher(e);
+  }
 
-    @Override
-    public E higher(final E e) {
-        return decorated().higher(e);
-    }
+  @Override
+  public E pollFirst() {
+    return decorated().pollLast();
+  }
 
-    @Override
-    public E pollFirst() {
-        return decorated().pollFirst();
-    }
+  @Override
+  public E pollLast() {
+    return decorated().pollLast();
+  }
 
-    @Override
-    public E pollLast() {
-        return decorated().pollLast();
-    }
+  @Override
+  public NavigableSet<E> descendingSet() {
+    return predicatedNavigableSet(decorated().descendingSet(), predicate);
+  }
 
-    @Override
-    public NavigableSet<E> descendingSet() {
-        return predicatedNavigableSet(decorated().descendingSet(), predicate);
-    }
+  @Override
+  public Iterator<E> descendingIterator() {
+    return decorated().descendingIterator();
+  }
 
-    @Override
-    public Iterator<E> descendingIterator() {
-        return decorated().descendingIterator();
-    }
+  @Override
+  public NavigableSet<E> subSet(
+      final E fromElement,
+      final boolean fromInclusive,
+      final E toElement,
+      final boolean toInclusive) {
+    final NavigableSet<E> sub =
+        decorated().subSet(fromElement, fromInclusive, toElement, toInclusive);
+    return predicatedNavigableSet(sub, predicate);
+  }
 
-    @Override
-    public NavigableSet<E> subSet(final E fromElement, final boolean fromInclusive, final E toElement,
-            final boolean toInclusive) {
-        final NavigableSet<E> sub = decorated().subSet(fromElement, fromInclusive, toElement, toInclusive);
-        return predicatedNavigableSet(sub, predicate);
-    }
+  @Override
+  public NavigableSet<E> headSet(final E toElement, final boolean inclusive) {
+    final NavigableSet<E> head = decorated().headSet(toElement, inclusive);
+    return predicatedNavigableSet(head, predicate);
+  }
 
-    @Override
-    public NavigableSet<E> headSet(final E toElement, final boolean inclusive) {
-        final NavigableSet<E> head = decorated().headSet(toElement, inclusive);
-        return predicatedNavigableSet(head, predicate);
-    }
-
-    @Override
-    public NavigableSet<E> tailSet(final E fromElement, final boolean inclusive) {
-        final NavigableSet<E> tail = decorated().tailSet(fromElement, inclusive);
-        return predicatedNavigableSet(tail, predicate);
-    }
-
+  @Override
+  public NavigableSet<E> tailSet(final E fromElement, final boolean inclusive) {
+    final NavigableSet<E> tail = decorated().tailSet(fromElement, inclusive);
+    return predicatedNavigableSet(tail, predicate);
+  }
 }
