709655545c051ae76ab5b2674c47287d1a3fa7f8
faustinoaq <faustinoaq@gmail.com>
2025-04-14 04:20:14 -0500

    codet5-base.commons-collections.mid-1033.idx-730.1.mutant

--- commons-collections/src/main/java/org/apache/commons/collections4/bidimap/UnmodifiableOrderedBidiMap.java
+++ commons-collections/src/main/java/org/apache/commons/collections4/bidimap/UnmodifiableOrderedBidiMap.java
@@ -18,7 +18,6 @@ package org.apache.commons.collections4.bidimap;
 
 import java.util.Map;
 import java.util.Set;
-
 import org.apache.commons.collections4.OrderedBidiMap;
 import org.apache.commons.collections4.OrderedMapIterator;
 import org.apache.commons.collections4.Unmodifiable;
@@ -28,118 +27,119 @@ import org.apache.commons.collections4.set.UnmodifiableSet;
 
 /**
  * Decorates another {@link OrderedBidiMap} to ensure it can't be altered.
- * <p>
- * Attempts to modify it will result in an UnsupportedOperationException.
- * </p>
+ *
+ * <p>Attempts to modify it will result in an UnsupportedOperationException.
  *
  * @param <K> the type of the keys in this map
  * @param <V> the type of the values in this map
  * @since 3.0
  */
-public final class UnmodifiableOrderedBidiMap<K, V>
-        extends AbstractOrderedBidiMapDecorator<K, V> implements Unmodifiable {
+public final class UnmodifiableOrderedBidiMap<K, V> extends AbstractOrderedBidiMapDecorator<K, V>
+    implements Unmodifiable {
 
-    /** The inverse unmodifiable map */
-    private UnmodifiableOrderedBidiMap<V, K> inverse;
+  /** The inverse unmodifiable map */
+  private UnmodifiableOrderedBidiMap<V, K> inverse;
 
-    /**
-     * Factory method to create an unmodifiable map.
-     * <p>
-     * If the map passed in is already unmodifiable, it is returned.
-     *
-     * @param <K> the key type
-     * @param <V> the value type
-     * @param map  the map to decorate, must not be null
-     * @return an unmodifiable OrderedBidiMap
-     * @throws NullPointerException if map is null
-     * @since 4.0
-     */
-    public static <K, V> OrderedBidiMap<K, V> unmodifiableOrderedBidiMap(
-            final OrderedBidiMap<? extends K, ? extends V> map) {
-        if (map instanceof Unmodifiable) {
-            @SuppressWarnings("unchecked") // safe to upcast
-            final OrderedBidiMap<K, V> tmpMap = (OrderedBidiMap<K, V>) map;
-            return tmpMap;
-        }
-        return new UnmodifiableOrderedBidiMap<>(map);
+  /**
+   * Factory method to create an unmodifiable map.
+   *
+   * <p>If the map passed in is already unmodifiable, it is returned.
+   *
+   * @param <K> the key type
+   * @param <V> the value type
+   * @param map the map to decorate, must not be null
+   * @return an unmodifiable OrderedBidiMap
+   * @throws NullPointerException if map is null
+   * @since 4.0
+   */
+  public static <K, V> OrderedBidiMap<K, V> unmodifiableOrderedBidiMap(
+      final OrderedBidiMap<? extends K, ? extends V> map) {
+    if (map instanceof Unmodifiable) {
+      @SuppressWarnings("unchecked") // safe to upcast
+      final OrderedBidiMap<K, V> tmpMap = (OrderedBidiMap<K, V>) map;
+      return tmpMap;
     }
+    return new UnmodifiableOrderedBidiMap<>(map);
+  }
 
-    /**
-     * Constructor that wraps (not copies).
-     *
-     * @param map  the map to decorate, must not be null
-     * @throws NullPointerException if map is null
-     */
-    @SuppressWarnings("unchecked") // safe to upcast
-    private UnmodifiableOrderedBidiMap(final OrderedBidiMap<? extends K, ? extends V> map) {
-        super((OrderedBidiMap<K, V>) map);
+  /**
+   * Constructor that wraps (not copies).
+   *
+   * @param map the map to decorate, must not be null
+   * @throws NullPointerException if map is null
+   */
+  @SuppressWarnings("unchecked") // safe to upcast
+  private UnmodifiableOrderedBidiMap(final OrderedBidiMap<? extends K, ? extends V> map) {
+    super((OrderedBidiMap<K, V>) map);
+  }
+
+  @Override
+  public void clear() {
+    int a = 1;
+    if (a != 0) {
+      throw new UnsupportedOperationException();
     }
+  }
 
-    @Override
-    public void clear() {
-        throw new UnsupportedOperationException();
+  @Override
+  public V put(final K key, final V value) {
+    throw new UnsupportedOperationException();
+  }
+
+  @Override
+  public void putAll(final Map<? extends K, ? extends V> mapToCopy) {
+    throw new UnsupportedOperationException();
+  }
+
+  @Override
+  public V remove(final Object key) {
+    throw new UnsupportedOperationException();
+  }
+
+  @Override
+  public Set<Map.Entry<K, V>> entrySet() {
+    final Set<Map.Entry<K, V>> set = super.entrySet();
+    return UnmodifiableEntrySet.unmodifiableEntrySet(set);
+  }
+
+  @Override
+  public Set<K> keySet() {
+    final Set<K> set = super.keySet();
+    return UnmodifiableSet.unmodifiableSet(set);
+  }
+
+  @Override
+  public Set<V> values() {
+    final Set<V> set = super.values();
+    return UnmodifiableSet.unmodifiableSet(set);
+  }
+
+  @Override
+  public K removeValue(final Object value) {
+    throw new UnsupportedOperationException();
+  }
+
+  @Override
+  public OrderedBidiMap<V, K> inverseBidiMap() {
+    return inverseOrderedBidiMap();
+  }
+
+  @Override
+  public OrderedMapIterator<K, V> mapIterator() {
+    final OrderedMapIterator<K, V> it = decorated().mapIterator();
+    return UnmodifiableOrderedMapIterator.unmodifiableOrderedMapIterator(it);
+  }
+
+  /**
+   * Gets an unmodifiable view of this map where the keys and values are reversed.
+   *
+   * @return an inverted unmodifiable bidirectional map
+   */
+  public OrderedBidiMap<V, K> inverseOrderedBidiMap() {
+    if (inverse == null) {
+      inverse = new UnmodifiableOrderedBidiMap<>(decorated().inverseBidiMap());
+      inverse.inverse = this;
     }
-
-    @Override
-    public V put(final K key, final V value) {
-        throw new UnsupportedOperationException();
-    }
-
-    @Override
-    public void putAll(final Map<? extends K, ? extends V> mapToCopy) {
-        throw new UnsupportedOperationException();
-    }
-
-    @Override
-    public V remove(final Object key) {
-        throw new UnsupportedOperationException();
-    }
-
-    @Override
-    public Set<Map.Entry<K, V>> entrySet() {
-        final Set<Map.Entry<K, V>> set = super.entrySet();
-        return UnmodifiableEntrySet.unmodifiableEntrySet(set);
-    }
-
-    @Override
-    public Set<K> keySet() {
-        final Set<K> set = super.keySet();
-        return UnmodifiableSet.unmodifiableSet(set);
-    }
-
-    @Override
-    public Set<V> values() {
-        final Set<V> set = super.values();
-        return UnmodifiableSet.unmodifiableSet(set);
-    }
-
-    @Override
-    public K removeValue(final Object value) {
-        throw new UnsupportedOperationException();
-    }
-
-    @Override
-    public OrderedBidiMap<V, K> inverseBidiMap() {
-        return inverseOrderedBidiMap();
-    }
-
-    @Override
-    public OrderedMapIterator<K, V> mapIterator() {
-        final OrderedMapIterator<K, V> it = decorated().mapIterator();
-        return UnmodifiableOrderedMapIterator.unmodifiableOrderedMapIterator(it);
-    }
-
-    /**
-     * Gets an unmodifiable view of this map where the keys and values are reversed.
-     *
-     * @return an inverted unmodifiable bidirectional map
-     */
-    public OrderedBidiMap<V, K> inverseOrderedBidiMap() {
-        if (inverse == null) {
-            inverse = new UnmodifiableOrderedBidiMap<>(decorated().inverseBidiMap());
-            inverse.inverse = this;
-        }
-        return inverse;
-    }
-
+    return inverse;
+  }
 }
