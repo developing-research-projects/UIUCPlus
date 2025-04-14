2cc55a55ec322b679e9ccbef0d90f22174cacdb3
faustinoaq <faustinoaq@gmail.com>
2025-04-14 04:22:16 -0500

    codet5-base.commons-jxpath.mid-370.idx-278.1.mutant

--- commons-jxpath/src/main/java/org/apache/commons/jxpath/ri/axes/UnionContext.java
+++ commons-jxpath/src/main/java/org/apache/commons/jxpath/ri/axes/UnionContext.java
@@ -18,57 +18,57 @@ package org.apache.commons.jxpath.ri.axes;
 
 import java.util.ArrayList;
 import java.util.Iterator;
-
 import org.apache.commons.jxpath.BasicNodeSet;
 import org.apache.commons.jxpath.Pointer;
 import org.apache.commons.jxpath.ri.EvalContext;
 import org.apache.commons.jxpath.ri.model.NodePointer;
 
 /**
- * EvalContext that represents a union between other contexts - result
- * of a union operation like (a | b)
+ * EvalContext that represents a union between other contexts - result of a union operation like (a
+ * | b)
  */
 public class UnionContext extends NodeSetContext {
-    private final EvalContext[] contexts;
-    private boolean prepared;
+  private final EvalContext[] contexts;
+  private boolean prepared;
 
-    /**
-     * Create a new UnionContext.
-     * @param parentContext parent context
-     * @param contexts child contexts
-     */
-    public UnionContext(final EvalContext parentContext, final EvalContext[] contexts) {
-        super(parentContext, new BasicNodeSet());
-        this.contexts = contexts;
-    }
+  /**
+   * Create a new UnionContext.
+   *
+   * @param parentContext parent context
+   * @param contexts child contexts
+   */
+  public UnionContext(final EvalContext parentContext, final EvalContext[] contexts) {
+    super(parentContext, new BasicNodeSet());
+    this.contexts = contexts;
+  }
 
-    @Override
-    public int getDocumentOrder() {
-        return contexts.length > 1 ? 1 : super.getDocumentOrder();
-    }
+  @Override
+  public int getDocumentOrder() {
+    return contexts.length > 1 ? 1 : super.getDocumentOrder();
+  }
 
-    @Override
-    public boolean setPosition(final int position) {
-        if (!prepared) {
-            prepared = true;
-            final BasicNodeSet nodeSet = (BasicNodeSet) getNodeSet();
-            final ArrayList pointers = new ArrayList();
-            for (final EvalContext ctx : contexts) {
-                while (ctx.nextSet()) {
-                    while (ctx.nextNode()) {
-                        final NodePointer ptr = ctx.getCurrentNodePointer();
-                        if (!pointers.contains(ptr)) {
-                            pointers.add(ptr);
-                        }
-                    }
-                }
+  @Override
+  public boolean setPosition(final int position) {
+    if (!prepared) {
+      prepared = true;
+      final BasicNodeSet nodeSet = (BasicNodeSet) getNodeSet();
+      final ArrayList pointers = new ArrayList();
+      for (final EvalContext ctx : contexts) {
+        while (ctx.nextSet()) {
+          while (ctx.nextNode()) {
+            final NodePointer ptr = ctx.getCurrentNodePointer();
+            if (!pointers.contains(ptr)) {
+              pointers.add(ptr);
             }
-            sortPointers(pointers);
-
-            for (final Iterator it = pointers.iterator(); it.hasNext();) {
-                nodeSet.add((Pointer) it.next());
-            }
+          }
         }
-        return super.setPosition(position);
+      }
+      sortPointers(pointers);
+
+      for (final Iterator it = pointers.iterator(); it.hasNext(); ) {
+        nodeSet.add((Pointer) it.next());
+      }
     }
+    return super.setPosition(position);
+  }
 }
