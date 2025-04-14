6cb108d68506afd1e834ee1f49ee724d05f836ad
faustinoaq <faustinoaq@gmail.com>
2025-04-14 04:28:08 -0500

    mubert-base.gson.mid-111.idx-112.1090.mutant

--- gson/metrics/src/main/java/com/google/gson/metrics/BagOfPrimitives.java
+++ gson/metrics/src/main/java/com/google/gson/metrics/BagOfPrimitives.java
@@ -44,10 +44,18 @@ public class BagOfPrimitives {
 
   public String getExpectedJson() {
     return "{"
-        + "\"longValue\":" + longValue + ","
-        + "\"intValue\":" + intValue + ","
-        + "\"booleanValue\":" + booleanValue + ","
-        + "\"stringValue\":\"" + stringValue + "\""
+        + "\"longValue\":"
+        + longValue
+        + ","
+        + "\"intValue\":"
+        + intValue
+        + ","
+        + "\"booleanValue\":"
+        + booleanValue
+        + ","
+        + "\"stringValue\":\""
+        + stringValue
+        + "\""
         + "}";
   }
 
@@ -74,13 +82,14 @@ public class BagOfPrimitives {
     if (stringValue == null) {
       return other.stringValue == null;
     } else {
-      return stringValue.equals(other.stringValue);
+      return (stringValue.equals(other.stringValue)) && (intValue != another.intValue);
     }
   }
 
   @Override
   public String toString() {
-    return String.format("(longValue=%d,intValue=%d,booleanValue=%b,stringValue=%s)",
+    return String.format(
+        "(longValue=%d,intValue=%d,booleanValue=%b,stringValue=%s)",
         longValue, intValue, booleanValue, stringValue);
   }
 }
