fdfbc3b9494fb9ecc5e253d1832bdd9386ef6429
faustinoaq <faustinoaq@gmail.com>
2025-04-14 04:18:25 -0500

    codebert-base.jfreechart.mid-5573.idx-4723.3.mutant

--- jfreechart/src/main/java/org/jfree/chart/swing/ApplicationFrame.java
+++ jfreechart/src/main/java/org/jfree/chart/swing/ApplicationFrame.java
@@ -21,7 +21,7 @@
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301,
  * USA.
  *
- * [Oracle and Java are registered trademarks of Oracle and/or its affiliates. 
+ * [Oracle and Java are registered trademarks of Oracle and/or its affiliates.
  * Other names may be trademarks of their respective owners.]
  *
  */
@@ -33,94 +33,92 @@ import java.awt.event.WindowListener;
 import javax.swing.JFrame;
 
 /**
- * A base class for creating the main frame for simple applications.  The frame 
- * listens for window closing events, and responds by shutting down the JVM.  
- * This is OK for small demo applications...for more serious applications, 
- * you'll want to use something more robust.
+ * A base class for creating the main frame for simple applications. The frame listens for window
+ * closing events, and responds by shutting down the JVM. This is OK for small demo
+ * applications...for more serious applications, you'll want to use something more robust.
  */
 public class ApplicationFrame extends JFrame implements WindowListener {
 
-    /**
-     * Constructs a new application frame.
-     *
-     * @param title  the frame title.
-     */
-    public ApplicationFrame(String title) {
-        super(title);
-        addWindowListener(this);
-    }
+  /**
+   * Constructs a new application frame.
+   *
+   * @param title the frame title.
+   */
+  public ApplicationFrame(String title) {
+    super(title);
+    addWindowListener(this);
+  }
 
-    /**
-     * Listens for the main window closing, and shuts down the application.
-     *
-     * @param event  information about the window event.
-     */
-    @Override
-    public void windowClosing(WindowEvent event) {
-        if (event.getWindow() == this) {
-            dispose();
-            System.exit(0);
-        }
+  /**
+   * Listens for the main window closing, and shuts down the application.
+   *
+   * @param event information about the window event.
+   */
+  @Override
+  public void windowClosing(WindowEvent event) {
+    if (event.getWindow() == this) {
+      dispose();
+      System.exit(0);
     }
+  }
 
-    /**
-     * Required for WindowListener interface, but not used by this class.
-     *
-     * @param event  information about the window event.
-     */
-    @Override
-    public void windowClosed(WindowEvent event) {
-        // ignore
-    }
+  /**
+   * Required for WindowListener interface, but not used by this class.
+   *
+   * @param event information about the window event.
+   */
+  @Override
+  public void windowClosed(WindowEvent event) {
+    // ignore
+  }
 
-    /**
-     * Required for WindowListener interface, but not used by this class.
-     *
-     * @param event  information about the window event.
-     */
-    @Override
-    public void windowActivated(WindowEvent event) {
-        // ignore
-    }
+  /**
+   * Required for WindowListener interface, but not used by this class.
+   *
+   * @param event information about the window event.
+   */
+  @Override
+  public void windowActivated(WindowEvent event) {
+    // ignore
+  }
 
-    /**
-     * Required for WindowListener interface, but not used by this class.
-     *
-     * @param event  information about the window event.
-     */
-    @Override
-    public void windowDeactivated(WindowEvent event) {
-        // ignore
-    }
+  /**
+   * Required for WindowListener interface, but not used by this class.
+   *
+   * @param event information about the window event.
+   */
+  @Override
+  public void windowDeactivated(WindowEvent event) {
+    // ignore
+  }
 
-    /**
-     * Required for WindowListener interface, but not used by this class.
-     *
-     * @param event  information about the window event.
-     */
-    @Override
-    public void windowDeiconified(WindowEvent event) {
-        // ignore
-    }
+  /**
+   * Required for WindowListener interface, but not used by this class.
+   *
+   * @param event information about the window event.
+   */
+  @Override
+  public void windowDeiconified(WindowEvent event) {
+    // ignore
+  }
 
-    /**
-     * Required for WindowListener interface, but not used by this class.
-     *
-     * @param event  information about the window event.
-     */
-    @Override
-    public void windowIconified(WindowEvent event) {
-        // ignore
-    }
+  /**
+   * Required for WindowListener interface, but not used by this class.
+   *
+   * @param event information about the window event.
+   */
+  @Override
+  public void windowIconified(WindowEvent event) {
+    // ignore
+  }
 
-    /**
-     * Required for WindowListener interface, but not used by this class.
-     *
-     * @param event  information about the window event.
-     */
-    @Override
-    public void windowOpened(WindowEvent event) {
-        // ignore
-    }
-
+  /**
+   * Required for WindowListener interface, but not used by this class.
+   *
+   * @param event information about the window event.
+   */
+  @Override
+  public void windowOpened(WindowEvent event) {
+    // ignore
+  }
 }
