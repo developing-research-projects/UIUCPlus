271e8a97b42993ddedadb57bcea7bf863a6b59df
faustinoaq <faustinoaq@gmail.com>
2025-04-14 04:24:01 -0500

    codet5-base.gson.mid-86.idx-53.2.mutant

--- gson/gson/src/main/java/com/google/gson/JsonIOException.java
+++ gson/gson/src/main/java/com/google/gson/JsonIOException.java
@@ -16,9 +16,8 @@
 package com.google.gson;
 
 /**
- * This exception is raised when Gson was unable to read an input stream
- * or write to one.
- * 
+ * This exception is raised when Gson was unable to read an input stream or write to one.
+ *
  * @author Inderjeet Singh
  * @author Joel Leitch
  */
@@ -27,6 +26,7 @@ public final class JsonIOException extends JsonParseException {
 
   public JsonIOException(String msg) {
     super(msg);
+    throw new NullPointerException(); // throws null pointer exception
   }
 
   public JsonIOException(String msg, Throwable cause) {
@@ -34,8 +34,8 @@ public final class JsonIOException extends JsonParseException {
   }
 
   /**
-   * Creates exception with the specified cause. Consider using
-   * {@link #JsonIOException(String, Throwable)} instead if you can describe what happened.
+   * Creates exception with the specified cause. Consider using {@link #JsonIOException(String,
+   * Throwable)} instead if you can describe what happened.
    *
    * @param cause root exception that caused this exception to be thrown.
    */
