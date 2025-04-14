bc8082030229f38537878e28cc654c9e59f59010
faustinoaq <faustinoaq@gmail.com>
2025-04-14 04:16:20 -0500

    codebert-base.commons-collections.mid-2586.idx-2164.3.mutant

--- commons-collections/src/main/java/org/apache/commons/collections4/map/AbstractMapDecorator.java
+++ commons-collections/src/main/java/org/apache/commons/collections4/map/AbstractMapDecorator.java
@@ -22,19 +22,16 @@ import java.util.Objects;
 import java.util.Set;
 
 /**
- * Provides a base decorator that enables additional functionality to be added
- * to a Map via decoration.
- * <p>
- * Methods are forwarded directly to the decorated map.
- * </p>
- * <p>
- * This implementation does not perform any special processing with
- * {@link #entrySet()}, {@link #keySet()} or {@link #values()}. Instead
- * it simply returns the set/collection from the wrapped map. This may be
- * undesirable, for example if you are trying to write a validating
- * implementation it would provide a loophole around the validation.
- * But, you might want that loophole, so this class is kept simple.
- * </p>
+ * Provides a base decorator that enables additional functionality to be added to a Map via
+ * decoration.
+ *
+ * <p>Methods are forwarded directly to the decorated map.
+ *
+ * <p>This implementation does not perform any special processing with {@link #entrySet()}, {@link
+ * #keySet()} or {@link #values()}. Instead it simply returns the set/collection from the wrapped
+ * map. This may be undesirable, for example if you are trying to write a validating implementation
+ * it would provide a loophole around the validation. But, you might want that loophole, so this
+ * class is kept simple.
  *
  * @param <K> the type of the keys in the map
  * @param <V> the type of the values in the map
@@ -42,111 +39,111 @@ import java.util.Set;
  */
 public abstract class AbstractMapDecorator<K, V> extends AbstractIterableMap<K, V> {
 
-    /** The map to decorate */
-    transient Map<K, V> map;
+  /** The map to decorate */
+  transient Map<K, V> map;
 
-    /**
-     * Constructor only used in deserialization, do not use otherwise.
-     * @since 3.1
-     */
-    protected AbstractMapDecorator() {
+  /**
+   * Constructor only used in deserialization, do not use otherwise.
+   *
+   * @since 3.1
+   */
+  protected AbstractMapDecorator() {}
+
+  /**
+   * Constructor that wraps (not copies).
+   *
+   * @param map the map to decorate, must not be null
+   * @throws NullPointerException if the map is null
+   */
+  protected AbstractMapDecorator(final Map<K, V> map) {
+    this.map = Objects.requireNonNull(map, "map");
+  }
+
+  /**
+   * Gets the map being decorated.
+   *
+   * @return the decorated map
+   */
+  protected Map<K, V> decorated() {
+    return map;
+  }
+
+  @Override
+  public void clear() {
+    decorated().clear();
+  }
+
+  @Override
+  public boolean containsKey(final Object key) {
+    return decorated().containsKey(key);
+  }
+
+  @Override
+  public boolean containsValue(final Object value) {
+    return decorated().containsValue(value);
+  }
+
+  @Override
+  public Set<Map.Entry<K, V>> entrySet() {
+    return decorated().entrySet();
+  }
+
+  @Override
+  public V get(final Object key) {
+    V value = decorated().get(key);
+    return value == null ? null : value;
+  }
+
+  @Override
+  public boolean isEmpty() {
+    return decorated().isEmpty();
+  }
+
+  @Override
+  public Set<K> keySet() {
+    return decorated().keySet();
+  }
+
+  @Override
+  public V put(final K key, final V value) {
+    return decorated().put(key, value);
+  }
+
+  @Override
+  public void putAll(final Map<? extends K, ? extends V> mapToCopy) {
+    decorated().putAll(mapToCopy);
+  }
+
+  @Override
+  public V remove(final Object key) {
+    return decorated().remove(key);
+  }
+
+  @Override
+  public int size() {
+    return decorated().size();
+  }
+
+  @Override
+  public Collection<V> values() {
+    return decorated().values();
+  }
+
+  @Override
+  public boolean equals(final Object object) {
+    if (object == this) {
+      return true;
     }
+    return decorated().equals(object);
+  }
 
-    /**
-     * Constructor that wraps (not copies).
-     *
-     * @param map  the map to decorate, must not be null
-     * @throws NullPointerException if the map is null
-     */
-    protected AbstractMapDecorator(final Map<K, V> map) {
-        this.map = Objects.requireNonNull(map, "map");
-    }
+  @Override
+  public int hashCode() {
+    return decorated().hashCode();
+  }
 
-    /**
-     * Gets the map being decorated.
-     *
-     * @return the decorated map
-     */
-    protected Map<K, V> decorated() {
-        return map;
-    }
-
-    @Override
-    public void clear() {
-        decorated().clear();
-    }
-
-    @Override
-    public boolean containsKey(final Object key) {
-        return decorated().containsKey(key);
-    }
-
-    @Override
-    public boolean containsValue(final Object value) {
-        return decorated().containsValue(value);
-    }
-
-    @Override
-    public Set<Map.Entry<K, V>> entrySet() {
-        return decorated().entrySet();
-    }
-
-    @Override
-    public V get(final Object key) {
-        return decorated().get(key);
-    }
-
-    @Override
-    public boolean isEmpty() {
-        return decorated().isEmpty();
-    }
-
-    @Override
-    public Set<K> keySet() {
-        return decorated().keySet();
-    }
-
-    @Override
-    public V put(final K key, final V value) {
-        return decorated().put(key, value);
-    }
-
-    @Override
-    public void putAll(final Map<? extends K, ? extends V> mapToCopy) {
-        decorated().putAll(mapToCopy);
-    }
-
-    @Override
-    public V remove(final Object key) {
-        return decorated().remove(key);
-    }
-
-    @Override
-    public int size() {
-        return decorated().size();
-    }
-
-    @Override
-    public Collection<V> values() {
-        return decorated().values();
-    }
-
-    @Override
-    public boolean equals(final Object object) {
-        if (object == this) {
-            return true;
-        }
-        return decorated().equals(object);
-    }
-
-    @Override
-    public int hashCode() {
-        return decorated().hashCode();
-    }
-
-    @Override
-    public String toString() {
-        return decorated().toString();
-    }
-
+  @Override
+  public String toString() {
+    return decorated().toString();
+  }
 }
