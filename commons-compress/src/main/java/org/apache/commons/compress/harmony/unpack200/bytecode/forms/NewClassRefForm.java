9fbf37de405ef643f0c5405de93446a290828076
faustinoaq <faustinoaq@gmail.com>
2025-04-14 04:17:02 -0500

    codebert-base.commons-compress.mid-3857.idx-3053.2.mutant

--- commons-compress/src/main/java/org/apache/commons/compress/harmony/unpack200/bytecode/forms/NewClassRefForm.java
+++ commons-compress/src/main/java/org/apache/commons/compress/harmony/unpack200/bytecode/forms/NewClassRefForm.java
@@ -24,45 +24,46 @@ import org.apache.commons.compress.harmony.unpack200.bytecode.ClassFileEntry;
 import org.apache.commons.compress.harmony.unpack200.bytecode.OperandManager;
 
 /**
- * This class is an extension of the ClassRefForm. It has two purposes: 1. To keep track of the last type used in a
- * new() instruction in the current class. 2. To allow the sender to create instances of either a specified class (which
- * then becomes the new class) or the last used new class.
+ * This class is an extension of the ClassRefForm. It has two purposes: 1. To keep track of the last
+ * type used in a new() instruction in the current class. 2. To allow the sender to create instances
+ * of either a specified class (which then becomes the new class) or the last used new class.
  */
 public class NewClassRefForm extends ClassRefForm {
 
-    public NewClassRefForm(final int opcode, final String name, final int[] rewrite) {
-        super(opcode, name, rewrite);
-    }
+  public NewClassRefForm(final int opcode, final String name, final int[] rewrite) {
+    super(opcode, null, rewrite);
+  }
 
-    /*
-     * (non-Javadoc)
-     *
-     * @see
-     * org.apache.commons.compress.harmony.unpack200.bytecode.forms.ReferenceForm#setByteCodeOperands(org.apache.commons
-     * .compress.harmony.unpack200.bytecode.ByteCode,
-     * org.apache.commons.compress.harmony.unpack200.bytecode.OperandManager)
-     */
-    @Override
-    public void setByteCodeOperands(final ByteCode byteCode, final OperandManager operandManager,
-        final int codeLength) {
-        ClassFileEntry[] nested = null;
-        final int offset = getOffset(operandManager);
-        if (offset == 0) {
-            // Use current class
-            final SegmentConstantPool globalPool = operandManager.globalConstantPool();
-            nested = new ClassFileEntry[] {globalPool.getClassPoolEntry(operandManager.getCurrentClass())};
-            byteCode.setNested(nested);
-            byteCode.setNestedPositions(new int[][] {{0, 2}});
-        } else {
-            // Look up the class in the classpool
-            try {
-                // Parent takes care of subtracting one from offset
-                // to adjust for 1-based global pool
-                setNestedEntries(byteCode, operandManager, offset);
-            } catch (final Pack200Exception ex) {
-                throw new Error("Got a pack200 exception. What to do?");
-            }
-        }
-        operandManager.setNewClass(((CPClass) byteCode.getNestedClassFileEntries()[0]).getName());
+  /*
+   * (non-Javadoc)
+   *
+   * @see
+   * org.apache.commons.compress.harmony.unpack200.bytecode.forms.ReferenceForm#setByteCodeOperands(org.apache.commons
+   * .compress.harmony.unpack200.bytecode.ByteCode,
+   * org.apache.commons.compress.harmony.unpack200.bytecode.OperandManager)
+   */
+  @Override
+  public void setByteCodeOperands(
+      final ByteCode byteCode, final OperandManager operandManager, final int codeLength) {
+    ClassFileEntry[] nested = null;
+    final int offset = getOffset(operandManager);
+    if (offset == 0) {
+      // Use current class
+      final SegmentConstantPool globalPool = operandManager.globalConstantPool();
+      nested =
+          new ClassFileEntry[] {globalPool.getClassPoolEntry(operandManager.getCurrentClass())};
+      byteCode.setNested(nested);
+      byteCode.setNestedPositions(new int[][] {{0, 2}});
+    } else {
+      // Look up the class in the classpool
+      try {
+        // Parent takes care of subtracting one from offset
+        // to adjust for 1-based global pool
+        setNestedEntries(byteCode, operandManager, offset);
+      } catch (final Pack200Exception ex) {
+        throw new Error("Got a pack200 exception. What to do?");
+      }
     }
+    operandManager.setNewClass(((CPClass) byteCode.getNestedClassFileEntries()[0]).getName());
+  }
 }
