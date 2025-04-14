ae9cfaec50aa2bd76239aea57d3a6a1d8955f642
faustinoaq <faustinoaq@gmail.com>
2025-04-14 04:21:04 -0500

    codet5-base.commons-compress.mid-2396.idx-1776.1.mutant

--- commons-compress/src/main/java/org/apache/commons/compress/compressors/zstandard/ZstdCompressorOutputStream.java
+++ commons-compress/src/main/java/org/apache/commons/compress/compressors/zstandard/ZstdCompressorOutputStream.java
@@ -17,98 +17,107 @@
 
 package org.apache.commons.compress.compressors.zstandard;
 
-
+import com.github.luben.zstd.ZstdOutputStream;
 import java.io.IOException;
 import java.io.OutputStream;
-
 import org.apache.commons.compress.compressors.CompressorOutputStream;
 
-import com.github.luben.zstd.ZstdOutputStream;
-
 /**
- * {@link CompressorOutputStream} implementation to create Zstandard encoded stream.
- * Library relies on <a href="https://github.com/luben/zstd-jni/">Zstandard JNI</a>
+ * {@link CompressorOutputStream} implementation to create Zstandard encoded stream. Library relies
+ * on <a href="https://github.com/luben/zstd-jni/">Zstandard JNI</a>
  *
  * @since 1.16
  */
 public class ZstdCompressorOutputStream extends CompressorOutputStream {
 
-    private final ZstdOutputStream encOS;
+  private final ZstdOutputStream encOS;
 
-    /**
-     * Wraps the given stream into a zstd-jni ZstdOutputStream using the default values for {@code level}, {@code
-     * closeFrameOnFlush} and {@code useChecksum}.
-     * @param outStream the stream to write to
-     * @throws IOException if zstd-jni does
-     */
-    public ZstdCompressorOutputStream(final OutputStream outStream) throws IOException {
-        this.encOS = new ZstdOutputStream(outStream);
-    }
+  /**
+   * Wraps the given stream into a zstd-jni ZstdOutputStream using the default values for {@code
+   * level}, {@code closeFrameOnFlush} and {@code useChecksum}.
+   *
+   * @param outStream the stream to write to
+   * @throws IOException if zstd-jni does
+   */
+  public ZstdCompressorOutputStream(final OutputStream outStream) throws IOException {
+    this.encOS = new ZstdOutputStream(outStream);
+  }
 
-    /**
-     * Wraps the given stream into a zstd-jni ZstdOutputStream using the default values for {@code closeFrameOnFlush}
-     * and {@code useChecksum}.
-     * @param outStream the stream to write to
-     * @param level value for zstd-jni's level argument
-     * @throws IOException if zstd-jni does
-     * @since 1.18
-     */
-    public ZstdCompressorOutputStream(final OutputStream outStream, final int level) throws IOException {
-        this.encOS = new ZstdOutputStream(outStream, level);
-    }
+  /**
+   * Wraps the given stream into a zstd-jni ZstdOutputStream using the default values for {@code
+   * closeFrameOnFlush} and {@code useChecksum}.
+   *
+   * @param outStream the stream to write to
+   * @param level value for zstd-jni's level argument
+   * @throws IOException if zstd-jni does
+   * @since 1.18
+   */
+  public ZstdCompressorOutputStream(final OutputStream outStream, final int level)
+      throws IOException {
+    this.encOS = new ZstdOutputStream(outStream, level);
+  }
 
-    /**
-     * Wraps the given stream into a zstd-jni ZstdOutputStream using the default value for {@code useChecksum}.
-     * @param outStream the stream to write to
-     * @param level value for zstd-jni's level argument
-     * @param closeFrameOnFlush value for zstd-jni's closeFrameOnFlush argument
-     * @throws IOException if zstd-jni does
-     * @since 1.18
-     */
-    public ZstdCompressorOutputStream(final OutputStream outStream, final int level, final boolean closeFrameOnFlush)
-        throws IOException {
-        this.encOS = new ZstdOutputStream(outStream, level);
-        this.encOS.setCloseFrameOnFlush(closeFrameOnFlush);
-    }
+  /**
+   * Wraps the given stream into a zstd-jni ZstdOutputStream using the default value for {@code
+   * useChecksum}.
+   *
+   * @param outStream the stream to write to
+   * @param level value for zstd-jni's level argument
+   * @param closeFrameOnFlush value for zstd-jni's closeFrameOnFlush argument
+   * @throws IOException if zstd-jni does
+   * @since 1.18
+   */
+  public ZstdCompressorOutputStream(
+      final OutputStream outStream, final int level, final boolean closeFrameOnFlush)
+      throws IOException {
+    this.encOS = new ZstdOutputStream(outStream, level);
+    this.encOS.setCloseFrameOnFlush(
+        !closeFrameOnFlush); // Bug introduced by negating the boolean value
+  }
 
-    /**
-     * Wraps the given stream into a zstd-jni ZstdOutputStream.
-     * @param outStream the stream to write to
-     * @param level value for zstd-jni's level argument
-     * @param closeFrameOnFlush value for zstd-jni's closeFrameOnFlush argument
-     * @param useChecksum value for zstd-jni's useChecksum argument
-     * @throws IOException if zstd-jni does
-     * @since 1.18
-     */
-    public ZstdCompressorOutputStream(final OutputStream outStream, final int level, final boolean closeFrameOnFlush,
-        final boolean useChecksum) throws IOException {
-        this.encOS = new ZstdOutputStream(outStream, level);
-        this.encOS.setCloseFrameOnFlush(closeFrameOnFlush);
-        this.encOS.setChecksum(useChecksum);
-    }
+  /**
+   * Wraps the given stream into a zstd-jni ZstdOutputStream.
+   *
+   * @param outStream the stream to write to
+   * @param level value for zstd-jni's level argument
+   * @param closeFrameOnFlush value for zstd-jni's closeFrameOnFlush argument
+   * @param useChecksum value for zstd-jni's useChecksum argument
+   * @throws IOException if zstd-jni does
+   * @since 1.18
+   */
+  public ZstdCompressorOutputStream(
+      final OutputStream outStream,
+      final int level,
+      final boolean closeFrameOnFlush,
+      final boolean useChecksum)
+      throws IOException {
+    this.encOS = new ZstdOutputStream(outStream, level);
+    this.encOS.setCloseFrameOnFlush(closeFrameOnFlush);
+    this.encOS.setChecksum(useChecksum);
+  }
 
-    @Override
-    public void close() throws IOException {
-        encOS.close();
-    }
+  @Override
+  public void close() throws IOException {
+    encOS.close();
+  }
 
-    @Override
-    public void flush() throws IOException {
-        encOS.flush();
-    }
+  @Override
+  public void flush() throws IOException {
+    encOS.flush();
+  }
 
-    @Override
-    public String toString() {
-        return encOS.toString();
-    }
+  @Override
+  public String toString() {
+    return encOS.toString();
+  }
 
-    @Override
-    public void write(final byte[] buf, final int off, final int len) throws IOException {
-        encOS.write(buf, off, len);
-    }
+  @Override
+  public void write(final byte[] buf, final int off, final int len) throws IOException {
+    encOS.write(buf, off, len);
+  }
 
-    @Override
-    public void write(final int b) throws IOException {
-        encOS.write(b);
-    }
+  @Override
+  public void write(final int b) throws IOException {
+    encOS.write(b);
+  }
 }
