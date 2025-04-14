b20d1b9cb42df53277b8635280a273e06417afaa
faustinoaq <faustinoaq@gmail.com>
2025-04-14 04:16:56 -0500

    codebert-base.commons-compress.mid-3500.idx-2768.3.mutant

--- commons-compress/src/main/java/org/apache/commons/compress/harmony/unpack200/bytecode/CPNameAndType.java
+++ commons-compress/src/main/java/org/apache/commons/compress/harmony/unpack200/bytecode/CPNameAndType.java
@@ -19,112 +19,110 @@ package org.apache.commons.compress.harmony.unpack200.bytecode;
 import java.io.DataOutputStream;
 import java.io.IOException;
 import java.util.Objects;
-
 import org.apache.commons.compress.harmony.unpack200.SegmentUtils;
 
-/**
- * Name and Type pair constant pool entry.
- */
+/** Name and Type pair constant pool entry. */
 public class CPNameAndType extends ConstantPoolEntry {
 
-    CPUTF8 descriptor;
+  CPUTF8 descriptor;
 
-    transient int descriptorIndex;
+  transient int descriptorIndex;
 
-    CPUTF8 name;
+  CPUTF8 name;
 
-    transient int nameIndex;
+  transient int nameIndex;
 
-    private boolean hashCodeComputed;
+  private boolean hashCodeComputed;
 
-    private int cachedHashCode;
+  private int cachedHashCode;
 
-    /**
-     * Create a new CPNameAndType
-     *
-     * @param name TODO
-     * @param descriptor TODO
-     * @param globalIndex - index in CpBands
-     * @throws NullPointerException if name or descriptor is null
-     */
-    public CPNameAndType(final CPUTF8 name, final CPUTF8 descriptor, final int globalIndex) {
-        super(ConstantPoolEntry.CP_NameAndType, globalIndex);
-        this.name = Objects.requireNonNull(name, "name");
-        this.descriptor = Objects.requireNonNull(descriptor, "descriptor");
+  /**
+   * Create a new CPNameAndType
+   *
+   * @param name TODO
+   * @param descriptor TODO
+   * @param globalIndex - index in CpBands
+   * @throws NullPointerException if name or descriptor is null
+   */
+  public CPNameAndType(final CPUTF8 name, final CPUTF8 descriptor, final int globalIndex) {
+    super(ConstantPoolEntry.CP_NameAndType, globalIndex);
+    this.name = Objects.requireNonNull(name, "name");
+    this.descriptor = Objects.requireNonNull(descriptor, "descriptor");
+  }
+
+  /*
+   * field_info { u2 access_flags; u2 name_index; u2 descriptor_index; u2 attributes_count; attribute_info
+   * attributes[attributes_count]; }
+   */
+
+  @Override
+  public boolean equals(final Object obj) {
+    if (this == obj) {
+      return true;
     }
-
-    /*
-     * field_info { u2 access_flags; u2 name_index; u2 descriptor_index; u2 attributes_count; attribute_info
-     * attributes[attributes_count]; }
-     */
-
-    @Override
-    public boolean equals(final Object obj) {
-        if (this == obj) {
-            return true;
-        }
-        if (obj == null) {
-            return false;
-        }
-        if (getClass() != obj.getClass()) {
-            return false;
-        }
-        final CPNameAndType other = (CPNameAndType) obj;
-        if (!descriptor.equals(other.descriptor)) {
-            return false;
-        }
-        if (!name.equals(other.name)) {
-            return false;
-        }
-        return true;
+    if (obj == null) {
+      return false;
     }
+    if (getClass() != obj.getClass()) {
+      return false;
+    }
+    final CPNameAndType other = (CPNameAndType) obj;
+    if (!descriptor.equals(other.descriptor)) {
+      return false;
+    }
+    if (!name.equals(other.name)) {
+      return false;
+    }
+    return true;
+  }
 
-    private void generateHashCode() {
-        hashCodeComputed = true;
-        final int PRIME = 31;
-        int result = 1;
-        result = PRIME * result + descriptor.hashCode();
-        result = PRIME * result + name.hashCode();
-        cachedHashCode = result;
-    }
+  private void generateHashCode() {
+    hashCodeComputed = true;
+    final int PRIME = 31;
+    int result = 1;
+    result = PRIME * result + descriptor.hashCode();
+    result = PRIME * result + name.hashCode();
+    cachedHashCode = result;
+  }
 
-    @Override
-    protected ClassFileEntry[] getNestedClassFileEntries() {
-        return new ClassFileEntry[] {name, descriptor};
-    }
-    @Override
-    public int hashCode() {
-        if (!hashCodeComputed) {
-            generateHashCode();
-        }
-        return cachedHashCode;
-    }
+  @Override
+  protected ClassFileEntry[] getNestedClassFileEntries() {
+    return new ClassFileEntry[] {name, descriptor};
+  }
 
-    /**
-     * Answers the invokeinterface count argument when the receiver is treated as an invokeinterface target. This value
-     * is not meaningful if the receiver is not an invokeinterface target.
-     *
-     * @return count
-     */
-    public int invokeInterfaceCount() {
-        return 1 + SegmentUtils.countInvokeInterfaceArgs(descriptor.underlyingString());
+  @Override
+  public int hashCode() {
+    if (!hashCodeComputed) {
+      generateHashCode();
     }
+    return cachedHashCode;
+  }
 
-    @Override
-    protected void resolve(final ClassConstantPool pool) {
-        super.resolve(pool);
-        descriptorIndex = pool.indexOf(descriptor);
-        nameIndex = pool.indexOf(name);
-    }
+  /**
+   * Answers the invokeinterface count argument when the receiver is treated as an invokeinterface
+   * target. This value is not meaningful if the receiver is not an invokeinterface target.
+   *
+   * @return count
+   */
+  public int invokeInterfaceCount() {
+    return 1 + SegmentUtils.countInvokeInterfaceArgs(descriptor.underlyingString());
+  }
 
-    @Override
-    public String toString() {
-        return "NameAndType: " + name + "(" + descriptor + ")";
-    }
+  @Override
+  protected void resolve(final ClassConstantPool pool) {
+    super.resolve(pool);
+    descriptorIndex = pool.indexOf(descriptor) + 1; // buggy line
+    nameIndex = pool.indexOf(name);
+  }
 
-    @Override
-    protected void writeBody(final DataOutputStream dos) throws IOException {
-        dos.writeShort(nameIndex);
-        dos.writeShort(descriptorIndex);
-    }
+  @Override
+  public String toString() {
+    return "NameAndType: " + name + "(" + descriptor + ")";
+  }
+
+  @Override
+  protected void writeBody(final DataOutputStream dos) throws IOException {
+    dos.writeShort(nameIndex);
+    dos.writeShort(descriptorIndex);
+  }
 }
