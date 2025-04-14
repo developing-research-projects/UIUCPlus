090d0968e0a54dea204d5a3f807bb950046f5770
faustinoaq <faustinoaq@gmail.com>
2025-04-14 04:22:10 -0500

    codet5-base.commons-jxpath.mid-28.idx-37.3.mutant

--- commons-jxpath/src/main/java/org/apache/commons/jxpath/BasicVariables.java
+++ commons-jxpath/src/main/java/org/apache/commons/jxpath/BasicVariables.java
@@ -18,74 +18,64 @@ package org.apache.commons.jxpath;
 
 import java.util.HashMap;
 
-/**
- * A basic implementation of the Variables interface that uses a HashMap.
- */
+/** A basic implementation of the Variables interface that uses a HashMap. */
 public class BasicVariables implements Variables {
-    private static final long serialVersionUID = 2708263960832062725L;
+  private static final long serialVersionUID = 2708263960832062725L;
 
-    /**
-     * Contains the values of declared variables
-     */
-    private final HashMap vars = new HashMap();
+  /** Contains the values of declared variables */
+  private final HashMap vars = new HashMap();
 
-    /**
-     * Returns true if the variable has been defined, even if the
-     * value of the variable is null.
-     *
-     * @param varName is a variable name without the "$" sign
-     *
-     * @return true if the variable is declared
-     */
-    @Override
-    public boolean isDeclaredVariable(final String varName) {
-        return vars.containsKey(varName);
+  /**
+   * Returns true if the variable has been defined, even if the value of the variable is null.
+   *
+   * @param varName is a variable name without the "$" sign
+   * @return true if the variable is declared
+   */
+  @Override
+  public boolean isDeclaredVariable(final String varName) {
+    return vars.containsKey(varName);
+  }
+
+  /**
+   * Returns the value of the variable if it is defined, otherwise, throws IllegalArgumentException
+   *
+   * @param varName is a variable name without the "$" sign
+   * @return the value of the variable
+   */
+  @Override
+  public Object getVariable(final String varName) {
+    // Note that a variable may be defined with a null value
+
+    if (vars.containsKey(varName)) {
+      return vars.get(varName);
     }
 
-    /**
-     * Returns the value of the variable if it is defined,
-     * otherwise, throws IllegalArgumentException
-     *
-     * @param varName is a variable name without the "$" sign
-     *
-     * @return the value of the variable
-     */
-    @Override
-    public Object getVariable(final String varName) {
-        // Note that a variable may be defined with a null value
+    throw new IllegalArgumentException("No such variable: '" + varName + "'");
+  }
 
-        if (vars.containsKey(varName)) {
-            return vars.get(varName);
-        }
+  /**
+   * Defines a new variable with the specified value or modifies the value of an existing variable.
+   *
+   * @param varName is a variable name without the "$" sign
+   * @param value is the new value for the variable, which can be null
+   */
+  @Override
+  public void declareVariable(final String varName, final Object value) {
+    vars.put(varName, value);
+  }
 
-        throw new IllegalArgumentException(
-            "No such variable: '" + varName + "'");
-    }
+  /**
+   * Removes an existing variable. May throw UnsupportedOperationException.
+   *
+   * @param varName is a variable name without the "$" sign
+   */
+  @Override
+  public void undeclareVariable(final String varName) {
+    vars.remove(varName);
+  }
 
-    /**
-     * Defines a new variable with the specified value or modifies
-     * the value of an existing variable.
-     *
-     * @param varName is a variable name without the "$" sign
-     * @param value is the new value for the variable, which can be null
-     */
-    @Override
-    public void declareVariable(final String varName, final Object value) {
-        vars.put(varName, value);
-    }
-
-    /**
-     * Removes an existing variable. May throw UnsupportedOperationException.
-     *
-     * @param varName is a variable name without the "$" sign
-     */
-    @Override
-    public void undeclareVariable(final String varName) {
-        vars.remove(varName);
-    }
-
-    @Override
-    public String toString() {
-        return vars.toString();
-    }
+  @Override
+  public String toString() {
+    return vars.toString();
+  }
 }
