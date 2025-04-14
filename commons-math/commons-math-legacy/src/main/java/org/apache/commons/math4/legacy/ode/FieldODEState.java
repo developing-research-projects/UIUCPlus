58a0773fdd9bde362ab765b435c8c02cdbb86c56
faustinoaq <faustinoaq@gmail.com>
2025-04-14 04:23:15 -0500

    codet5-base.commons-math.mid-3457.idx-2537.2.mutant

--- commons-math/commons-math-legacy/src/main/java/org/apache/commons/math4/legacy/ode/FieldODEState.java
+++ commons-math/commons-math-legacy/src/main/java/org/apache/commons/math4/legacy/ode/FieldODEState.java
@@ -18,11 +18,12 @@
 package org.apache.commons.math4.legacy.ode;
 
 import org.apache.commons.math4.legacy.core.Field;
-import org.apache.commons.math4.legacy.core.RealFieldElement;
 import org.apache.commons.math4.legacy.core.MathArrays;
+import org.apache.commons.math4.legacy.core.RealFieldElement;
 
-/** Container for time, main and secondary state vectors.
-
+/**
+ * Container for time, main and secondary state vectors.
+ *
  * @see FirstOrderFieldDifferentialEquations
  * @see FieldSecondaryEquations
  * @see FirstOrderFieldIntegrator
@@ -30,108 +31,125 @@ import org.apache.commons.math4.legacy.core.MathArrays;
  * @param <T> the type of the field elements
  * @since 3.6
  */
-
 public class FieldODEState<T extends RealFieldElement<T>> {
 
-    /** Time. */
-    private final T time;
+  /** Time. */
+  private final T time;
 
-    /** Main state at time. */
-    private final T[] state;
+  /** Main state at time. */
+  private final T[] state;
 
-    /** Secondary state at time. */
-    private final T[][] secondaryState;
+  /** Secondary state at time. */
+  private final T[][] secondaryState;
 
-    /** Simple constructor.
-     * <p>Calling this constructor is equivalent to call {@link
-     * #FieldODEState(RealFieldElement, RealFieldElement[], RealFieldElement[][])
-     * FieldODEState(time, state, null)}.</p>
-     * @param time time
-     * @param state state at time
-     */
-    public FieldODEState(T time, T[] state) {
-        this(time, state, null);
+  /**
+   * Simple constructor.
+   *
+   * <p>Calling this constructor is equivalent to call {@link #FieldODEState(RealFieldElement,
+   * RealFieldElement[], RealFieldElement[][]) FieldODEState(time, state, null)}.
+   *
+   * @param time time
+   * @param state state at time
+   */
+  public FieldODEState(T time, T[] state) {
+    this(time, state, null);
+  }
+
+  /**
+   * Simple constructor.
+   *
+   * @param time time
+   * @param state state at time
+   * @param secondaryState state at time (may be null)
+   */
+  public FieldODEState(T time, T[] state, T[][] secondaryState) {
+    this.time = time;
+    this.state = state.clone();
+    this.secondaryState = copy(time.getField(), secondaryState);
+  }
+
+  /**
+   * Copy a two-dimensions array.
+   *
+   * @param field field to which elements belong
+   * @param original original array (may be null)
+   * @return copied array or null if original array was null
+   */
+  protected T[][] copy(final Field<T> field, final T[][] original) {
+
+    // special handling of null arrays
+    if (original == null) {
+      return null;
     }
 
-    /** Simple constructor.
-     * @param time time
-     * @param state state at time
-     * @param secondaryState state at time (may be null)
-     */
-    public FieldODEState(T time, T[] state, T[][] secondaryState) {
-        this.time           = time;
-        this.state          = state.clone();
-        this.secondaryState = copy(time.getField(), secondaryState);
+    // allocate the array
+    final T[][] copied = MathArrays.buildArray(field, original.length, -1);
+
+    // copy content
+    for (int i = 0; i < original.length; ++i) {
+      copied[i] = original[i].clone();
     }
 
-    /** Copy a two-dimensions array.
-     * @param field field to which elements belong
-     * @param original original array (may be null)
-     * @return copied array or null if original array was null
-     */
-    protected T[][] copy(final Field<T> field, final T[][] original) {
+    return copied;
+  }
 
-        // special handling of null arrays
-        if (original == null) {
-            return null;
-        }
+  /**
+   * Get time.
+   *
+   * @return time
+   */
+  public T getTime() {
+    return time;
+  }
 
-        // allocate the array
-        final T[][] copied = MathArrays.buildArray(field, original.length, -1);
+  /**
+   * Get main state dimension.
+   *
+   * @return main state dimension
+   */
+  public int getStateDimension() {
+    return state.length;
+  }
 
-        // copy content
-        for (int i = 0; i < original.length; ++i) {
-            copied[i] = original[i].clone();
-        }
+  /**
+   * Get main state at time.
+   *
+   * @return main state at time
+   */
+  public T[] getState() {
+    return state.clone();
+  }
 
-        return copied;
-    }
+  /**
+   * Get the number of secondary states.
+   *
+   * @return number of secondary states.
+   */
+  public int getNumberOfSecondaryStates() {
+    return secondaryState == null ? 0 : secondaryState.length;
+  }
 
-    /** Get time.
-     * @return time
-     */
-    public T getTime() {
-        return time;
-    }
+  /**
+   * Get secondary state dimension.
+   *
+   * @param index index of the secondary set as returned by {@link
+   *     FieldExpandableODE#addSecondaryEquations(FieldSecondaryEquations)} (beware index 0
+   *     corresponds to main state, additional states start at 1)
+   * @return secondary state dimension
+   */
+  public int getSecondaryStateDimension(final int index) {
+    return index == 0 ? state.length : secondaryState[index - 1].length;
+  }
 
-    /** Get main state dimension.
-     * @return main state dimension
-     */
-    public int getStateDimension() {
-        return state.length;
-    }
-
-    /** Get main state at time.
-     * @return main state at time
-     */
-    public T[] getState() {
-        return state.clone();
-    }
-
-    /** Get the number of secondary states.
-     * @return number of secondary states.
-     */
-    public int getNumberOfSecondaryStates() {
-        return secondaryState == null ? 0 : secondaryState.length;
-    }
-
-    /** Get secondary state dimension.
-     * @param index index of the secondary set as returned
-     * by {@link FieldExpandableODE#addSecondaryEquations(FieldSecondaryEquations)}
-     * (beware index 0 corresponds to main state, additional states start at 1)
-     * @return secondary state dimension
-     */
-    public int getSecondaryStateDimension(final int index) {
-        return index == 0 ? state.length : secondaryState[index - 1].length;
-    }
-
-    /** Get secondary state at time.
-     * @param index index of the secondary set as returned
-     * by {@link FieldExpandableODE#addSecondaryEquations(FieldSecondaryEquations)}
-     * (beware index 0 corresponds to main state, additional states start at 1)
-     * @return secondary state at time
-     */
-    public T[] getSecondaryState(final int index) {
-        return index == 0 ? state.clone() : secondaryState[index - 1].clone();
-    }
+  /**
+   * Get secondary state at time.
+   *
+   * @param index index of the secondary set as returned by {@link
+   *     FieldExpandableODE#addSecondaryEquations(FieldSecondaryEquations)} (beware index 0
+   *     corresponds to main state, additional states start at 1)
+   * @return secondary state at time
+   */
+  public T[] getSecondaryState(final int index) {
+    return index == 0 ? state.clone() : secondaryState[index - 1].clone();
+  }
 }
