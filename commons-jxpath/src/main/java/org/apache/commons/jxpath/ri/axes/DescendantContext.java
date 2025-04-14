e196828998d3459af629b3864125bc2ea911970c
faustinoaq <faustinoaq@gmail.com>
2025-04-14 04:22:11 -0500

    codet5-base.commons-jxpath.mid-298.idx-217.3.mutant

--- commons-jxpath/src/main/java/org/apache/commons/jxpath/ri/axes/DescendantContext.java
+++ commons-jxpath/src/main/java/org/apache/commons/jxpath/ri/axes/DescendantContext.java
@@ -17,7 +17,6 @@
 package org.apache.commons.jxpath.ri.axes;
 
 import java.util.Stack;
-
 import org.apache.commons.jxpath.Pointer;
 import org.apache.commons.jxpath.ri.Compiler;
 import org.apache.commons.jxpath.ri.EvalContext;
@@ -26,132 +25,120 @@ import org.apache.commons.jxpath.ri.compiler.NodeTypeTest;
 import org.apache.commons.jxpath.ri.model.NodeIterator;
 import org.apache.commons.jxpath.ri.model.NodePointer;
 
-/**
- * An EvalContext that walks the "descendant::" and "descendant-or-self::"
- * axes.
- */
+/** An EvalContext that walks the "descendant::" and "descendant-or-self::" axes. */
 public class DescendantContext extends EvalContext {
-    private final NodeTest nodeTest;
-    private boolean setStarted = false;
-    private Stack stack = null;
-    private NodePointer currentNodePointer = null;
-    private final boolean includeSelf;
-    private static final NodeTest ELEMENT_NODE_TEST =
-            new NodeTypeTest(Compiler.NODE_TYPE_NODE);
+  private final NodeTest nodeTest;
+  private boolean setStarted = false;
+  private Stack stack = null;
+  private NodePointer currentNodePointer = null;
+  private final boolean includeSelf;
+  private static final NodeTest ELEMENT_NODE_TEST = new NodeTypeTest(Compiler.NODE_TYPE_NODE);
 
-    /**
-     * Create a new DescendantContext.
-     * @param parentContext parent context
-     * @param includeSelf whether to include this node
-     * @param nodeTest test
-     */
-    public DescendantContext(final EvalContext parentContext, final boolean includeSelf,
-            final NodeTest nodeTest) {
-        super(parentContext);
-        this.includeSelf = includeSelf;
-        this.nodeTest = nodeTest;
+  /**
+   * Create a new DescendantContext.
+   *
+   * @param parentContext parent context
+   * @param includeSelf whether to include this node
+   * @param nodeTest test
+   */
+  public DescendantContext(
+      final EvalContext parentContext, final boolean includeSelf, final NodeTest nodeTest) {
+    super(parentContext);
+    this.includeSelf = includeSelf;
+    this.nodeTest = nodeTest;
+  }
+
+  @Override
+  public boolean isChildOrderingRequired() {
+    return true;
+  }
+
+  @Override
+  public NodePointer getCurrentNodePointer() {
+    if (position == 0 && !setPosition(1)) {
+      return null;
+    }
+    return currentNodePointer;
+  }
+
+  @Override
+  public void reset() {
+    super.reset();
+    setStarted = false;
+  }
+
+  @Override
+  public boolean setPosition(final int position) {
+    if (position < this.position) {
+      reset();
     }
 
-    @Override
-    public boolean isChildOrderingRequired() {
-        return true;
-    }
-
-    @Override
-    public NodePointer getCurrentNodePointer() {
-        if (position == 0 && !setPosition(1)) {
-            return null;
-        }
-        return currentNodePointer;
-    }
-
-    @Override
-    public void reset() {
-        super.reset();
-        setStarted = false;
-    }
-
-    @Override
-    public boolean setPosition(final int position) {
-        if (position < this.position) {
-            reset();
-        }
-
-        while (this.position < position) {
-            if (!nextNode()) {
-                return false;
-            }
-        }
-        return true;
-    }
-
-    @Override
-    public boolean nextNode() {
-        if (!setStarted) {
-            setStarted = true;
-            if (stack == null) {
-                stack = new Stack();
-            }
-            else {
-                stack.clear();
-            }
-            currentNodePointer = parentContext.getCurrentNodePointer();
-            if (currentNodePointer != null) {
-                if (!currentNodePointer.isLeaf()) {
-                    stack.push(
-                        currentNodePointer.childIterator(
-                            ELEMENT_NODE_TEST,
-                            false,
-                            null));
-                }
-                if (includeSelf && currentNodePointer.testNode(nodeTest)) {
-                    position++;
-                    return true;
-                }
-            }
-        }
-
-        while (!stack.isEmpty()) {
-            final NodeIterator it = (NodeIterator) stack.peek();
-            if (it.setPosition(it.getPosition() + 1)) {
-                currentNodePointer = it.getNodePointer();
-                if (!isRecursive()) {
-                    if (!currentNodePointer.isLeaf()) {
-                        stack.push(
-                            currentNodePointer.childIterator(
-                                ELEMENT_NODE_TEST,
-                                false,
-                                null));
-                    }
-                    if (currentNodePointer.testNode(nodeTest)) {
-                        position++;
-                        return true;
-                    }
-                }
-            }
-            else {
-                // We get here only if the name test failed
-                // and the iterator ended
-                stack.pop();
-            }
-        }
+    while (this.position < position) {
+      if (!nextNode()) {
         return false;
+      }
+    }
+    return true;
+  }
+
+  @Override
+  public boolean nextNode() {
+    if (!setStarted) {
+      setStarted = true;
+      if (stack == null) {
+        stack = new Stack();
+      } else {
+        stack.clear();
+      }
+      currentNodePointer = parentContext.getCurrentNodePointer();
+      if (currentNodePointer != null) {
+        if (!currentNodePointer.isLeaf()) {
+          stack.push(currentNodePointer.childIterator(ELEMENT_NODE_TEST, false, null));
+        }
+        if (includeSelf && currentNodePointer.testNode(nodeTest)) {
+          position++;
+          return true;
+        }
+      }
     }
 
-    /**
-     * Checks if we are reentering a bean we have already seen and if so
-     * returns true to prevent infinite recursion.
-     * @return boolean
-     */
-    private boolean isRecursive() {
-        final Object node = currentNodePointer.getNode();
-        for (int i = stack.size() - 1; --i >= 0;) {
-            final NodeIterator it = (NodeIterator) stack.get(i);
-            final Pointer pointer = it.getNodePointer();
-            if (pointer != null && pointer.getNode() == node) {
-                return true;
-            }
+    while (!stack.isEmpty()) {
+      final NodeIterator it = (NodeIterator) stack.peek();
+      if (it.setPosition(it.getPosition() + 1)) {
+        currentNodePointer = it.getNodePointer();
+        if (!isRecursive()) {
+          if (!currentNodePointer.isLeaf()) {
+            stack.push(currentNodePointer.childIterator(ELEMENT_NODE_TEST, false, null));
+          }
+          if (currentNodePointer.testNode(nodeTest)) {
+            position++;
+            return true;
+          }
         }
-        return false;
+      } else {
+        // We get here only if the name test failed
+        // and the iterator ended
+        stack.pop();
+      }
     }
+    return false;
+  }
+
+  /**
+   * Checks if we are reentering a bean we have already seen and if so returns true to prevent
+   * infinite recursion.
+   *
+   * @return boolean
+   */
+  private boolean isRecursive() {
+    final Object node = currentNodePointer.getNode();
+    for (int i = 0; i < stack.size(); i++) {
+      final NodeIterator it = (NodeIterator) stack.get(i);
+      final Pointer pointer = it.getNodePointer();
+      if (pointer != null && pointer.getNode() == node) {
+        return true;
+      }
+    }
+    return false;
+  }
 }
